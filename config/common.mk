02fb1699072b0e3363fc6d6545b7f46c61d7d8e7
Chirayu Desai <chirayudesai1@gmail.com>
2018-10-25 22:36:06 +0530

    Include temporary vendor, for prebuilt apps

--- config/common.mk
+++ config/common.mk
@@ -1,2 +1,5 @@
 PRODUCT_COPY_FILES += \
 	vendor/calyx/prebuilt/bootanimation.zip:system/media/bootanimation.zip
+
+# Temporary, for prebuilt apps
+$(call inherit-product, vendor/calyx-tmp/common.mk)
