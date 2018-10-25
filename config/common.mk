a0f8ea275f079fb6af7214ad3d5b4923fbbbf1ad
Chirayu Desai <chirayudesai1@gmail.com>
2018-10-25 23:31:13 +0530

    Build F-Droid and PrivelegedExtension

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,10 @@
 PRODUCT_COPY_FILES += \
 	vendor/calyx/prebuilt/bootanimation.zip:system/media/bootanimation.zip
 
+# Apps
+PRODUCT_PACKAGES += \
+	F-Droid \
+	F-DroidPrivelegedExtension
+
 # Temporary, for prebuilt apps
 $(call inherit-product, vendor/calyx-tmp/common.mk)
