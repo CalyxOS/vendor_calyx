406f49883f00fb3262a2ed1445d8833886a01908
Chirayu Desai <chirayudesai1@gmail.com>
2018-12-13 22:58:09 +0530

    Update release script
    * Support signing releases outside the build environment,
      using otatools.zip + target-files.zip (and vendor-board-info.txt)
    * Usage: release.sh device [target-files.zip]

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,69 +1,73 @@
 #!/bin/bash
+# Script to sign a target files package, and generate ota packages and factory images
+# Refer to https://source.android.com/devices/tech/ota/sign_builds for more details
 
-user_error() {
-  echo user error, please replace user and try again >&2
+error() {
+  echo error: $1, please try again >&2
+  echo "Usage: $0 device [target-files.zip]"
   exit 1
 }
 
-[[ $# -eq 1 ]] || user_error
-[[ -n $BUILD_NUMBER ]] || user_error
-
-KEY_DIR=keys/$1
-OUT=out/release-$1-$BUILD_NUMBER
+[[ $# -le 2 ]] || error "incorrect number of arguments"
+[[ -n $BUILD_NUMBER ]] || error "BUILD_NUMBER not set, did you source envsetup.sh ?"
 
 source device/common/clear-factory-images-variables.sh
 
-get_radio_image() {
+DEVICE=$1
+PRODUCT=$1
+KEY_DIR=keys/$DEVICE
+OUT=out/release-$DEVICE-$BUILD_NUMBER
+BUILD=$BUILD_NUMBER
+SIGNED_TARGET_FILES=$OUT/$DEVICE-target_files-$BUILD.zip
+
+if [[ -z $2 ]] ; then
+  TARGET_FILES=out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/calyx_$DEVICE-target_files-$BUILD_NUMBER.zip
+  RELEASETOOLS_PATH=build/tools
+else
+  TARGET_FILES=$2
+  # For usage with otatools.zip
+  RELEASETOOLS_PATH=.
+fi
+
+VERSION=$(unzip -c $TARGET_FILES SYSTEM/build.prop | grep "ro.build.id=" | cut -d = -f 2 | tr '[:upper:]' '[:lower:]')
+
+get_radio_image_version() {
   grep -Po "require version-$1=\K.+" vendor/$2/vendor-board-info.txt | tr '[:upper:]' '[:lower:]'
 }
 
-if [[ $1 == marlin || $1 == sailfish || $1 == taimen || $1 == walleye || $1 == blueline || $1 == crosshatch ]]; then
-  BOOTLOADER=$(get_radio_image bootloader google_devices/$1)
-  RADIO=$(get_radio_image baseband google_devices/$1)
-  PREFIX=aosp_
-elif [[ $1 == hikey || $1 == hikey960 ]]; then
-  :
+if [[ $DEVICE == marlin || $DEVICE == sailfish || $DEVICE == taimen || $DEVICE == walleye ]]; then
+  BOOTLOADER=$(get_radio_image_version bootloader google_devices/$DEVICE)
+  RADIO=$(get_radio_image_version baseband google_devices/$DEVICE)
+elif [[ $DEVICE == jasmine ]]; then
+  : # TODO
 else
-  user_error
+  error "Unsupported device $DEVICE"
 fi
-
-BUILD=$BUILD_NUMBER
-VERSION=$(grep -Po "export BUILD_ID=\K.+" build/core/build_id.mk | tr '[:upper:]' '[:lower:]')
-DEVICE=$1
-PRODUCT=$1
 
 mkdir -p $OUT || exit 1
 
-TARGET_FILES=$DEVICE-target_files-$BUILD.zip
-
-if [[ $DEVICE != hikey* ]]; then
-  if [[ $DEVICE == marlin || $DEVICE == sailfish ]]; then
-    VERITY_SWITCHES=(--replace_verity_public_key "$KEY_DIR/verity_key.pub" --replace_verity_private_key "$KEY_DIR/verity"
-                     --replace_verity_keyid "$KEY_DIR/verity.x509.pem")
-  else
-    VERITY_SWITCHES=(--avb_vbmeta_key "$KEY_DIR/avb.pem" --avb_vbmeta_algorithm SHA256_RSA2048)
-    AVB_PKMD="$PWD/$KEY_DIR/avb_pkmd.bin"
-  fi
+if [[ $DEVICE == marlin || $DEVICE == sailfish || $DEVICE == jasmine ]]; then
+  VERITY_SWITCHES=(--replace_verity_public_key "$KEY_DIR/verity_key.pub" --replace_verity_private_key "$KEY_DIR/verity"
+                   --replace_verity_keyid "$KEY_DIR/verity.x509.pem")
+elif [[ $DEVICE == taimen || $DEVICE == walleye ]]; then
+  VERITY_SWITCHES=(--avb_vbmeta_key "$KEY_DIR/avb.pem" --avb_vbmeta_algorithm SHA256_RSA2048)
+  AVB_PKMD="$PWD/$KEY_DIR/avb_pkmd.bin"
 fi
 
-build/tools/releasetools/sign_target_files_apks -o -d "$KEY_DIR" "${VERITY_SWITCHES[@]}" \
-  out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/$PREFIX$DEVICE-target_files-$BUILD_NUMBER.zip \
-  $OUT/$TARGET_FILES || exit 1
+echo "Creating signed targetfiles zip"
+$RELEASETOOLS_PATH/releasetools/sign_target_files_apks -p . -o -d "$KEY_DIR" "${VERITY_SWITCHES[@]}" \
+  $TARGET_FILES $SIGNED_TARGET_FILES || exit 1
 
-if [[ $DEVICE != hikey* ]]; then
-  build/tools/releasetools/ota_from_target_files --block -k "$KEY_DIR/releasekey" "${EXTRA_OTA[@]}" $OUT/$TARGET_FILES \
-    $OUT/$DEVICE-ota_update-$BUILD.zip || exit 1
-fi
+echo "Create OTA update zip"
+$RELEASETOOLS_PATH/releasetools/ota_from_target_files -p . -k "$KEY_DIR/releasekey" "${EXTRA_OTA[@]}" $TARGET_FILES \
+  $OUT/$DEVICE-ota_update-$BUILD.zip || exit 1
 
-build/tools/releasetools/img_from_target_files $OUT/$TARGET_FILES \
+echo "Creating factory images"
+$RELEASETOOLS_PATH/releasetools/img_from_target_files -p . $SIGNED_TARGET_FILES \
   $OUT/$DEVICE-img-$BUILD.zip || exit 1
 
 cd $OUT || exit 1
 
-if [[ $DEVICE == hikey* ]]; then
-  source ../../device/linaro/hikey/factory-images/generate-factory-images-$DEVICE.sh
-else
-  source ../../device/common/generate-factory-images-common.sh
-fi
+source ../../device/common/generate-factory-images-common.sh
 
 mv $DEVICE-$VERSION-factory-*.zip $DEVICE-factory-$BUILD_NUMBER.zip
