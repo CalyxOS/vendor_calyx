7371c5be142f3f80690ae560e85724415ebd767e
Chirayu Desai <chirayudesai1@gmail.com>
2018-12-17 17:10:31 +0530

    Add incremental OTA generation script
    * Based on https://github.com/AndroidHardening/script/blob/6894d7610c15f5c05fa76ec9f971c7f99ec0c569/generate_delta.sh

---
+++ scripts/generate_delta.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+error() {
+  echo error: $1, please try again >&2
+  echo "Usage: $0 device [target-files.zip]"
+  exit 1
+}
+
+[[ $# -eq 3 ]] ||  error "incorrect number of arguments"
+
+DEVICE=$1
+KEY_DIR=keys/$DEVICE
+OLD=$2
+NEW=$3
+
+if [[ -d build ]]; then
+  RELEASETOOLS_PATH=build/tools
+else
+  # For usage with otatools.zip
+  RELEASETOOLS_PATH=.
+fi
+
+$RELEASETOOLS_PATH/releasetools/ota_from_target_files -p . -k "$KEY_DIR/releasekey" \
+  -i archive/release-$DEVICE-$OLD/$DEVICE-target_files-$OLD.zip \
+  archive/release-$DEVICE-$NEW/$DEVICE-target_files-$NEW.zip \
+  archive/release-$DEVICE-$NEW/$DEVICE-incremental-$OLD-$NEW.zip
