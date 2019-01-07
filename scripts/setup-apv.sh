7625a7fa44755af1ef3004a127514a5579b930c7
Chirayu Desai <chirayudesai1@gmail.com>
2019-01-07 17:21:00 +0530

    Add script to setup vendor files

---
+++ scripts/setup-apv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Setup android-prepare-vendor for all supported devices
+
+SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
+
+source $SCRIPTPATH/metadata
+
+APV=vendor/android-prepare-vendor
+
+for device in ${devices[@]}; do 
+	echo "Setting up vendor for $device ${buildid[$device]}"
+	$APV/execute-all.sh -y --debugfs -d $device -b ${buildid[$device]} -o $APV || exit 1
+done
+
+rm -rf vendor/google_devices && mkdir vendor/google_devices
+
+for device in ${devices[@]}; do
+	cp -a $APV/$device/${buildid[$device]}/vendor/google_devices vendor/
+	rm -rf $APV/$device/${buildid[$device]}/vendor*
+done
+
+# git -C $APV clean -fdx
