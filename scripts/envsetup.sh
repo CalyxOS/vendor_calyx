ecee4dc661594ca24d13658e4aab86b4a6f625aa
Chirayu Desai <chirayudesai1@gmail.com>
2019-06-08 00:52:32 +0530

    envsetup: export CALYX_BUILD=true

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -5,4 +5,5 @@ export _JAVA_OPTIONS=-XX:-UsePerfData
 export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date --utc +%Y.%m.%d.%H)
 echo "BUILD_NUMBER=$BUILD_NUMBER"
 export DISPLAY_BUILD_NUMBER=true
+export CALYX_BUILD=true
 chrt -b -p 0 $$
