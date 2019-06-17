2376160b2324d44e587bccd384279943406186fc
Chirayu Desai <chirayudesai1@gmail.com>
2019-06-18 01:12:47 +0530

    Add script to update the local F-Droid repo
    * Fetches the latest artifacts from the CI job at
      https://gitlab.com/calyxos/calyxos-fdroid-repo
      and updates the prebuilts repository

---
+++ scripts/fdroid-repo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Update the local CalyxOS F-Droid repository
+
+SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
+DATE=$(date +%F)
+FDROIDREPOTMP=$(mktemp -d)
+JOBID=$(curl -sI https://gitlab.com/calyxos/calyxos-fdroid-repo/-/jobs/artifacts/master/download?job=fdroid-repo | grep Location | cut -d / -f 8)
+
+pushd $SCRIPTPATH/../../../prebuilts/calyx/
+repo start fdroid-update-${DATE} . || exit 1
+pushd $FDROIDREPOTMP
+curl -L https://gitlab.com/calyxos/calyxos-fdroid-repo/-/jobs/${JOBID}/artifacts/download -o artifacts.zip || exit 1
+unzip artifacts.zip
+popd
+rm -rf fdroid/repo
+cp -a ${FDROIDREPOTMP}/public/fdroid/repo fdroid/
+git add fdroid/repo
+git commit -m "Update fdroid repo to ${JOBID}"
+rm -rf ${FDROIDREPOTMP}
+popd
