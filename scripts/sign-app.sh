b083c5c6e466597f4df974789a3f83e2d36d280b
Chirayu Desai <chirayudesai1@gmail.com>
2019-06-21 22:10:10 +0530

    Add script to sign apps

---
+++ scripts/sign-app.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Signs given app with the appropriate key
+
+SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
+
+source $SCRIPTPATH/metadata
+
+KEY_DIR=keys
+AAPT=bin/aapt
+SIGNAPK=framework/signapk.jar
+LIB=lib64
+
+error() {
+  echo error: $1, please try again >&2
+  echo "Usage: $0 <app> file.apk"
+  echo "Supported apps: ${apps[@]}"
+  exit 1
+}
+
+[[ $# -eq 2 ]] || error "incorrect number of arguments"
+
+APP=${1}
+APK=${2}
+if [[ ! " ${apps[@]} " =~ " ${APP} " ]]; then
+	error "Unsupported app ${APP}"
+fi
+
+APPKEY=${appkey[$APP]}
+KEY=$KEY_DIR/${keymap[$APPKEY]}
+SDK=$($AAPT dump badging $APK | grep sdkVersion | cut -d \' -f 2)
+
+java -Djava.library.path=${LIB} -jar $SIGNAPK --min-sdk-version ${SDK} ${KEY}.x509.pem ${KEY}.pk8 ${APK} ${APP}-signed.apk
