2b6173f6865464ed755816150953ad8da53096e4
Michael Bestas <mkbestas@lineageos.org>
2019-08-10 23:43:28 +0300

    config: Include charging sounds in notification sounds
    ... in order to make them selectable in charging sounds settings.
    This way the user can go back to the default sound.
    
    Change-Id: Iced4614baddd9033e0772e4a4221ed48dea6fc57

--- config/audio.mk
+++ config/audio.mk
@@ -9,6 +9,10 @@ else
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
 endif
 
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/sounds/effects/ogg/ChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/ChargingStarted.ogg \
+    frameworks/base/data/sounds/effects/material/ogg/WirelessChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/WirelessChargingStarted.ogg
+
 # Default sounds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.alarm_alert=Oxygen.ogg \
