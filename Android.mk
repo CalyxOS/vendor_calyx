2778d48abbfb9aa0ba422efd195c15946a5b003a
Chirayu Desai <chirayudesai1@gmail.com>
2019-09-12 02:06:22 +0530

    Fix repo-manifest generation

--- Android.mk
+++ Android.mk
@@ -2,11 +2,13 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := repo-manifest
-LOCAL_MODULE_CLASS := FAKE
-include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/Android.mk
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
+LOCAL_SRC_FILES := repo-manifest
+include $(BUILD_PREBUILT)
 $(LOCAL_BUILT_MODULE):
-	$(hide) repo manifest -r -o $(TARGET_OUT_ETC)/repo-manifest.xml
+	$(hide) $(shell repo manifest -r -o $(TARGET_OUT_ETC)/repo-manifest.xml)
 	$(hide) touch $@
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
