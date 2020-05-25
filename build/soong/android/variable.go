a7071ef971f1b49794e139256420f8b6c642ec1b
dianlujitao <dianlujitao@lineageos.org>
2020-05-25 18:08:31 +0800

    Export qcom UM hal root to soong
    Change-Id: Iabb75e8745dba9069c2eab0d5ef41b2171f4c048

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,6 +3,10 @@ type Product_variables struct {
 	Additional_gralloc_10_usage_bits struct {
 		Cppflags []string
 	}
+	Qcom_um_soong_namespace struct {
+		Cflags []string
+		Header_libs []string
+	}
 	Should_wait_for_qsee struct {
 		Cflags []string
 	}
@@ -25,6 +29,7 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
+	Qcom_um_soong_namespace  *string `json:",omitempty"`
 	Should_wait_for_qsee  *bool `json:",omitempty"`
 	Supports_extended_compress_format  *bool `json:",omitempty"`
 	Supports_hw_fde  *bool `json:",omitempty"`
