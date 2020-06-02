d1ea3ff6d7615b1d77170aeeed4cbdee87968ead
Chirayu Desai <chirayudesai1@gmail.com>
2020-06-03 04:36:33 +0530

    generate_metadata: Support target-files, and optional channel argument
    * Also add help text

--- scripts/generate_metadata.py
+++ scripts/generate_metadata.py
@@ -4,12 +4,37 @@ from argparse import ArgumentParser
 from zipfile import ZipFile
 
 parser = ArgumentParser(description="Generate update server metadata")
-parser.add_argument("zip")
+parser.add_argument("zip", help="ota_update or target_files")
+parser.add_argument("channel", default="testing", nargs='?', help="ota channel (default: testing)")
+
+# From build/make/tools/releasetools/common.py
+def LoadBuildProp(input_file, prop_file):
+    lines = input_file.read(prop_file).decode().split("\n")
+    d = {}
+    for line in lines:
+      line = line.strip()
+      if not line or line.startswith("#"):
+        continue
+      if "=" in line:
+        name, value = line.split("=", 1)
+        d[name] = value
+    return d
 
 with ZipFile(parser.parse_args().zip) as f:
-    with f.open("META-INF/com/android/metadata") as metadata:
-        data = dict(line[:-1].decode().split("=") for line in metadata)
-        with open(data["pre-device"] + "-testing", "w") as output:
-            build_id = data["post-build"].split("/")[3]
-            incremental = data["post-build"].split("/")[4].split(":")[0]
-            print(incremental, data["post-timestamp"], build_id, file=output)
+    if "META-INF/com/android/metadata" in f.namelist():
+        with f.open("META-INF/com/android/metadata") as metadata:
+            data = dict(line[:-1].decode().split("=") for line in metadata)
+            with open(data["pre-device"] + "-" + parser.parse_args().channel, "w") as output:
+                build_id = data["post-build"].split("/")[3]
+                incremental = data["post-build"].split("/")[4].split(":")[0]
+                print(incremental, data["post-timestamp"], build_id, file=output)
+    elif "SYSTEM/build.prop" in f.namelist():
+        data = LoadBuildProp(f, "SYSTEM/build.prop")
+        with open(data["ro.product.system.device"] + "-" + parser.parse_args().channel, "w") as output:
+            build_id = data["ro.build.id"]
+            incremental = data["ro.build.version.incremental"]
+            timestamp = data["ro.build.date.utc"]
+            print(incremental, timestamp, build_id, file=output)
+    else:
+        print("Unsupported file: " + parser.parse_args().zip)
+        parser.print_help()
