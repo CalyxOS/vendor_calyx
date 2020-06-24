28f0086d31d0ab483d1c778e92b75b48b609c181
Chirayu Desai <chirayudesai1@gmail.com>
2020-06-25 04:49:28 +0530

    Add key encryption and decryption scripts
    * From https://github.com/GrapheneOS/script/tree/7d4a7bbf0d77199ee8acc6f76cae0e30e34cca85
    * Allows changing password of existing keys
    * Allows switching to more secure key formats
    * Encryption script is meant to be run once on existing keys
    * Decryption script is ran automatically when signing, aka only meant for internal use.
    
    Change-Id: Ie5451a20a3a8880642af1a4c8fcbd2803ac0b49f

---
+++ scripts/encrypt_keys.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+
+source "$(dirname ${BASH_SOURCE[0]})/common.sh"
+
+[[ $# -ne 1 ]] && user_error "expected 1 argument (key directory)"
+
+cd $1
+
+read -p "Enter old key passphrase (empty if none): " -s password
+echo
+
+read -p "Enter new key passphrase: " -s new_password
+echo
+read -p "Confirm new key passphrase: " -s confirm_new_password
+echo
+
+if [[ $new_password != $confirm_new_password ]]; then
+    echo new password does not match
+    exit 1
+fi
+
+tmp="$(mktemp -d /dev/shm/encrypt_keys.XXXXXXXXXX)"
+trap "rm -rf \"$tmp\"" EXIT
+
+export password
+export new_password
+
+for key in releasekey platform shared media networkstack; do
+    if [[ -n $password ]]; then
+        openssl pkcs8 -inform DER -in $key.pk8 -passin env:password | openssl pkcs8 -topk8 -outform DER -out "$tmp/$key.pk8" -passout env:new_password -scrypt
+    else
+        openssl pkcs8 -topk8 -inform DER -in $key.pk8 -outform DER -out "$tmp/$key.pk8" -passout env:new_password -scrypt
+    fi
+done
+
+if [[ -f avb.pem ]]; then
+    if [[ -n $password ]]; then
+        openssl pkcs8 -topk8 -in avb.pem -passin env:password -out "$tmp/avb.pem" -passout env:new_password -scrypt
+    else
+        openssl pkcs8 -topk8 -in avb.pem -out "$tmp/avb.pem" -passout env:new_password -scrypt
+    fi
+fi
+
+unset password
+unset new_password
+
+mv "$tmp"/* .
