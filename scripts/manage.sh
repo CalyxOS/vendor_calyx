8e43b003a8f8930822a36a4db0f3b9d7e8b4601f
Chirayu Desai <chirayudesai1@gmail.com>
2020-07-01 01:19:55 +0530

    manage: remove unused code

--- scripts/manage.sh
+++ scripts/manage.sh
@@ -6,15 +6,6 @@ SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
 
 source $SCRIPTPATH/metadata
 
-DELETE_TAG=
-
-build_number=
-if [[ $# -eq 1 ]]; then
-  build_number=$1
-elif [[ $# -ne 0 ]]; then
-  exit 1
-fi
-
 wait_for_conflict() {
   echo -e "\n>>> $(tput setaf 3)Please resolve conflicts in $1$(tput sgr0)"
   echo -e ">>> $(tput setaf 3)Press enter when done$(tput sgr0)"
@@ -28,29 +19,11 @@ for repo in "${aosp_forks[@]}"; do
   git fetch gitlab-priv
   git checkout -b $branch gitlab-priv/$prev_branch || #exit 1
 
-  if [[ -n $DELETE_TAG ]]; then
-    git tag -d $DELETE_TAG
-    git push gitlab-priv :refs/tags/$DELETE_TAG
-    cd .. || exit 1
-    continue
-  fi
+  git fetch aosp --tags || exit 1
 
-  if [[ -n $build_number ]]; then
-    echo "Not pushing tags for now"
-    #git tag -s $aosp_version.$build_number -m $aosp_version.$build_number || exit 1
-    #git push gitlab-priv $aosp_version.$build_number || exit 1
-
-    if [[ $repo == platform_manifest ]]; then
-      git checkout $branch || exit 1
-      git branch -D tmp || exit 1
-    fi
-  else
-    git fetch aosp --tags || exit 1
-
-    git pull --rebase=interactive aosp $aosp_tag || wait_for_conflict $repo
-    git push -f gitlab-priv HEAD:refs/heads/$branch || exit 1
-    git push -f gitlab-priv $aosp_tag:refs/tags/$aosp_tag || exit 1
-  fi
+  git pull --rebase=interactive aosp $aosp_tag || wait_for_conflict $repo
+  git push -f gitlab-priv HEAD:refs/heads/$branch || exit 1
+  git push -f gitlab-priv $aosp_tag:refs/tags/$aosp_tag || exit 1
 
   cd .. || exit 1
 done
@@ -62,28 +35,15 @@ for kernel in "${!kernels[@]}"; do
   git fetch gitlab-priv
   git checkout -b $branch gitlab-priv/$prev_branch || exit 1
 
-  if [[ -n $DELETE_TAG ]]; then
-    git tag -d $DELETE_TAG
-    git push gitlab-priv :refs/tags/$DELETE_TAG
+  git fetch aosp --tags || exit 1
+  kernel_tag=${kernels[$kernel]}
+  if [[ -z $kernel_tag ]]; then
     cd .. || exit 1
     continue
   fi
-
-  if [[ -n $build_number ]]; then
-    echo "Not pushing tags for now"
-    #git tag -s $aosp_version.$build_number -m $aosp_version.$build_number || exit 1
-    #git push gitlab-priv $aosp_version.$build_number || exit 1
-  else
-    git fetch aosp --tags || exit 1
-    kernel_tag=${kernels[$kernel]}
-    if [[ -z $kernel_tag ]]; then
-      cd .. || exit 1
-      continue
-    fi
-    git rebase --interactive $kernel_tag || wait_for_conflict $kernel
-    git push -f gitlab-priv HEAD:refs/heads/$branch || exit 1
-    git push -f gitlab-priv $kernel_tag:refs/tags/$kernel_tag || exit 1
-  fi
+  git rebase --interactive $kernel_tag || wait_for_conflict $kernel
+  git push -f gitlab-priv HEAD:refs/heads/$branch || exit 1
+  git push -f gitlab-priv $kernel_tag:refs/tags/$kernel_tag || exit 1
 
   cd .. || exit 1
 done
@@ -91,39 +51,11 @@ done
 for repo in ${independent[@]} ${lineage_forks[@]}; do
   echo -e "\n>>> $(tput setaf 3)Handling $repo$(tput sgr0)"
 
-  if [[ $repo == platform_manifest ]]; then
-    echo "TODO"
-  fi
-
   cd $repo || exit 1
   git fetch gitlab-priv
   git checkout -b $branch gitlab-priv/$prev_branch || exit 1
 
-  if [[ -n $DELETE_TAG ]]; then
-    git tag -d $DELETE_TAG
-    git push gitlab-priv :refs/tags/$DELETE_TAG
-    cd .. || exit 1
-    continue
-  fi
-
-  if [[ -n $build_number ]]; then
-    if [[ $repo == platform_manifest ]]; then
-      git checkout -B tmp || exit 1
-      sed -i s%refs/heads/$branch%refs/tags/$aosp_version.$build_number% default.xml || exit 1
-      git commit default.xml -m $aosp_version.$build_number || exit 1
-    fi
-
-    echo "Not pushing tags for now"
-    #git tag -s $aosp_version.$build_number -m $aosp_version.$build_number || exit 1
-    #git push gitlab-priv $aosp_version.$build_number || exit 1
-
-    if [[ $repo == platform_manifest ]]; then
-      git checkout $branch || exit 1
-      git branch -D tmp || exit 1
-    fi
-  else
-    git push -f gitlab-priv HEAD:refs/heads/$branch || exit 1
-  fi
+  git push -f gitlab-priv HEAD:refs/heads/$branch || exit 1
 
   cd .. || exit 1
 done
