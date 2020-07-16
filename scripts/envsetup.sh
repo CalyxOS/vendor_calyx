d896e8d15aa61570eabcfdf0cf89b6c0be3362c9
Chirayu Desai <chirayudesai1@gmail.com>
2020-07-16 21:45:20 +0530

    envsetup: Only set build number for official builds

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -2,8 +2,12 @@ source build/envsetup.sh
 
 export LANG=en_US.UTF-8
 export _JAVA_OPTIONS=-XX:-UsePerfData
+if [[ -n $OFFICIAL_BUILD ]]; then
 export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date --utc +%Y.%m.%d.%H)
 echo "BUILD_NUMBER=$BUILD_NUMBER"
 export DISPLAY_BUILD_NUMBER=true
+else
+echo "NOT an official build"
+fi
 export CALYX_BUILD=true
 chrt -b -p 0 $$
