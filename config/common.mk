ee0564d42010161f8192d00cd6caa2ee4fab16a9
Chirayu Desai <chirayudesai1@gmail.com>
2020-08-27 04:34:12 +0530

    config: Google Camera for sunfish

--- config/common.mk
+++ config/common.mk
@@ -18,7 +18,7 @@ PRODUCT_PACKAGES += \
 endif
 
 # For Google Camera
-ifeq ($(filter-out taimen walleye crosshatch blueline bonito sargo coral flame, $(VENDOR_DEVICE)),)
+ifeq ($(filter-out taimen walleye crosshatch blueline bonito sargo coral flame sunfish, $(VENDOR_DEVICE)),)
 PRODUCT_COPY_FILES += \
 	vendor/calyx/prebuilt/google_experience.xml:system/etc/sysconfig/google_experience.xml
 endif
