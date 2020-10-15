e346a8cfdb0170561ae55b8ab3ec44cb18d1a606
Chirayu Desai <chirayudesai1@gmail.com>
2020-10-16 05:09:30 +0530

    setup-apv: vendor/google_devices is a git repo now, do not delete
    Change-Id: Idca3ed0026b08e554e57d667929a335e686028b3

--- scripts/setup-apv.sh
+++ scripts/setup-apv.sh
@@ -7,12 +7,12 @@ source $SCRIPTPATH/metadata
 
 APV=vendor/android-prepare-vendor
 
-for device in ${devices[@]}; do 
+for device in ${devices[@]}; do
 	echo "Setting up vendor for $device ${buildid[$device]}"
 	$APV/execute-all.sh -y --debugfs -d $device -b ${buildid[$device]} -o $APV || exit 1
 done
 
-rm -rf vendor/google_devices && mkdir vendor/google_devices
+rm -rf vendor/google_devices/*
 
 for device in ${devices[@]}; do
 	cp -a $APV/$device/${buildid[$device]}/vendor/google_devices vendor/
