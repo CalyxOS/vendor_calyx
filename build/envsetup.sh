08a2e063147e525908024b6f4844a7f4a2aaa9cb
Michael Bestas <mkbestas@gmail.com>
2020-11-07 20:17:57 +0200

    Move & update envsetup.sh
    * Move to proper directory
    * Add help option and remove unneeded exports and commands
    
    Change-Id: I1b5bcaf6861c2866675c943cc53bab352f2e955f

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,16 +1,20 @@
-source build/envsetup.sh
+function __print_calyx_functions_help() {
+cat <<EOF
+Additional CalyxOS functions:
+- aospremote:      Add git remote for matching AOSP repository.
+- calyxremote:     Add git remote for CalyxOS Gerrit Review.
+- repopick:        Utility to fetch changes from Gerrit.
+EOF
+}
 
-export LANG=en_US.UTF-8
-export _JAVA_OPTIONS=-XX:-UsePerfData
 if [[ -n $OFFICIAL_BUILD ]]; then
-export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date --utc +%Y.%m.%d.%H)
+export BUILD_NUMBER=$(cat $OUT_DIR/build_number.txt 2>/dev/null || date --utc +%Y.%m.%d.%H)
 echo "BUILD_NUMBER=$BUILD_NUMBER"
 export DISPLAY_BUILD_NUMBER=true
 else
 echo "NOT an official build"
 fi
 export CALYX_BUILD=true
-chrt -b -p 0 $$
 
 function aospremote()
 {
