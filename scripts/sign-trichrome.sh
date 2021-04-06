a14baa4d8ac58e714631fdd7e776faf57c7d9c08
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-07 05:28:52 +0530

    sign-trichrome: Support invocation from an arbitrary path
    * Not the cleanest, but does the job
    * Used to sign APKs for all the arches
    
    Change-Id: I376bd5b271fb4348c0b00e53b0b8bceb9f6da8dd

--- scripts/sign-trichrome.sh
+++ scripts/sign-trichrome.sh
@@ -2,14 +2,15 @@
 # Signs the Trichrome apks
 
 SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
+TOP=$SCRIPTPATH/../../../
 
 source $SCRIPTPATH/metadata
 
-KEY_DIR=keys/common
-APKSIGNER=bin/apksigner
+KEY_DIR=$TOP/keys/common
+APKSIGNER=$TOP/bin/apksigner
 KEY=${KEY_DIR}/calyxos.keystore
-BUNDLETOOL="bundletool-all-1.4.0.jar"
-AAPT2=aapt2
+BUNDLETOOL=$TOP/"bundletool-all-1.4.0.jar"
+AAPT2=$TOP/aapt2
 
 error() {
   echo error: $1, please try again >&2
@@ -30,7 +31,7 @@ error() {
 [[ -e TrichromeChrome.apks ]] && error "TrichromeChrome.apks already exists"
 [[ -e TrichromeChrome.apk ]] && error "TrichromeChrome.apk already exists"
 
-java -jar ${BUNDLETOOL} build-apks --bundle TrichromeChrome.aab --output TrichromeChrome.apks --mode=universal --aapt2 $SCRIPTPATH/../../../$AAPT2 --ks ${KEY} --ks-key-alias calyxos
+java -jar ${BUNDLETOOL} build-apks --bundle TrichromeChrome.aab --output TrichromeChrome.apks --mode=universal --aapt2 $AAPT2 --ks ${KEY} --ks-key-alias calyxos
 unzip TrichromeChrome.apks universal.apk && mv universal.apk TrichromeChrome-signed.apk
 
 for APP in TrichromeLibrary TrichromeWebView; do
