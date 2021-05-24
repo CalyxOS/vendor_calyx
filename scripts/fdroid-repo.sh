c73eab0a6e5339aeefe68f308c5eed8dc8daa625
Chirayu Desai <chirayudesai1@gmail.com>
2021-05-25 03:41:29 +0530

    Sign F-Droid with our own key [1/2]
    * Currently just testkey / releasekey as that's what we have the infra for
    * Could do an F-Droid specific key in the future to make updates easier
    
    Issue: calyxos#295
    Change-Id: Ib4ee566d9b6320b3bffe4eaa445b2075f11e3219

--- scripts/fdroid-repo.sh
+++ scripts/fdroid-repo.sh
@@ -31,7 +31,7 @@ APPS=$(for apk in $APKS; do echo ${apk%.*}; done)
 	echo -e 'include $(CLEAR_VARS)'
 	echo -e 'LOCAL_MODULE := F-Droid'
 	echo -e 'LOCAL_SRC_FILES := F-Droid.apk'
-	echo -e 'LOCAL_CERTIFICATE := PRESIGNED'
+	echo -e 'LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)'
 	echo -e 'LOCAL_MODULE_CLASS := APPS'
 	echo -e 'include $(BUILD_PREBUILT)'
 	echo
