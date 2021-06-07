d7c20b78ff7f875cc8e7da8d0ace4d62fdf6c67b
Chirayu Desai <chirayudesai1@gmail.com>
2021-06-07 19:07:22 +0530

    Add common key creation scripts
    * This could be cleaned up, but for now it's fine, as it won't
      interfere with any existing keys and just happily create any new
      keys we need.
    * Keeping keys for app like these common helps with updates, and also
      specifically for F-Droid it helps with privext too
    
    Change-Id: I2481cbf4ae22339aeef253bbaf4433e3082cf269

---
+++ scripts/mkcommonkeys.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+error() {
+  echo error: $1, please try again >&2
+  echo "Usage: $0 key_dir subject"
+  echo "Example subject: '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'"
+  exit 1
+}
+
+[[ $# -eq 2 ]] ||  error "incorrect number of arguments"
+
+SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
+TOP="$SCRIPTPATH/../../.."
+
+source $SCRIPTPATH/metadata
+
+KEY_DIR=$1
+SUBJECT="$2"
+
+[[ ! -e $(which openssl) ]] && error "openssl not found in PATH."
+
+[[ -d $KEY_DIR ]] && error "key directory already exists"
+mkdir -p $KEY_DIR
+
+pushd $KEY_DIR
+
+$SCRIPTPATH/mkkeystore.sh calyxos "$SUBJECT"
+
+for k in "${common_app_keys[@]}"; do
+  $SCRIPTPATH/mkkey.sh "$k" "$SUBJECT"
+done
+
+popd
