a4f9ab816d0b197a022f9120ab10944871db7ea8
Michael Bestas <mkbestas@gmail.com>
2021-06-11 22:03:52 +0300

    Relocate fdroid prebuilts to product partition
    Change-Id: Id9ffbc9916a796ace458f923c0f2c97d8fb3e0f2

--- scripts/fdroid-repo.sh
+++ scripts/fdroid-repo.sh
@@ -33,15 +33,17 @@ APPS=$(for apk in $APKS; do echo ${apk%.*}; done)
 	echo -e 'LOCAL_SRC_FILES := F-Droid.apk'
 	echo -e 'LOCAL_CERTIFICATE := PRESIGNED'
 	echo -e 'LOCAL_MODULE_CLASS := APPS'
+	echo -e "LOCAL_PRODUCT_MODULE := true"
 	echo -e 'include $(BUILD_PREBUILT)'
 	echo
 	echo -e 'include $(CLEAR_VARS)'
 	echo -e "LOCAL_MODULE := fdroid-repo"
 	echo -e "LOCAL_MODULE_CLASS := ETC"
 	echo -e "LOCAL_MODULE_TAGS := optional"
-	echo -e 'LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/org.fdroid.fdroid'
+	echo -e 'LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/org.fdroid.fdroid'
 	echo -e "LOCAL_MODULE_STEM := additional_repos.xml"
 	echo -e "LOCAL_SRC_FILES := additional_repos.xml"
+	echo -e "LOCAL_PRODUCT_MODULE := true"
 	echo -e 'include $(BUILD_PREBUILT)'
 	echo
 } > $ANDROID_MK
