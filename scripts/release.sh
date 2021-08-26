ad759d43933b9d1aaaaf29ea98189b781b271240
Chirayu Desai <chirayudesai1@gmail.com>
2021-08-27 02:34:48 +0530

    Hello, Pixel 5a!
    Change-Id: I27c7ced3f53802f235da209f229dfc58140cf0c2

--- scripts/release.sh
+++ scripts/release.sh
@@ -39,7 +39,7 @@ VERSION=$(unzip -c $TARGET_FILES SYSTEM/build.prop | grep "ro.build.id=" | cut -
 if [[ $DEVICE == marlin || $DEVICE == sailfish || $DEVICE == taimen || $DEVICE == walleye ||
 	$DEVICE == blueline || $DEVICE == crosshatch || $DEVICE == sargo || $DEVICE == bonito ||
 	$DEVICE == coral || $DEVICE == flame || $DEVICE == sunfish ||
-  $DEVICE == redfin || $DEVICE == bramble ]]; then
+  $DEVICE == redfin || $DEVICE == bramble || $DEVICE == barbet ]]; then
   BOOTLOADER=$(unzip -c $TARGET_FILES VENDOR/build.prop | grep "ro.build.expect.bootloader=" | cut -d = -f 2)
   RADIO=$(unzip -c $TARGET_FILES VENDOR/build.prop | grep "ro.build.expect.baseband=" | cut -d = -f 2)
 elif [[ $DEVICE == jasmine_sprout ]]; then
@@ -64,11 +64,15 @@ elif [[ $DEVICE == coral || $DEVICE == flame || $DEVICE == sunfish ||
   VERITY_SWITCHES=(--avb_vbmeta_key "$KEY_DIR/avb.pem" --avb_vbmeta_algorithm SHA256_RSA2048
                    --avb_system_key "$KEY_DIR/avb.pem" --avb_system_algorithm SHA256_RSA2048
                    --avb_vbmeta_system_key "$KEY_DIR/avb.pem" --avb_vbmeta_system_algorithm SHA256_RSA2048)
+elif [[ $DEVICE == barbet ]]; then
+  VERITY_SWITCHES=(--avb_vbmeta_key "$KEY_DIR/avb.pem" --avb_vbmeta_algorithm SHA256_RSA4096
+                   --avb_system_key "$KEY_DIR/avb.pem" --avb_system_algorithm SHA256_RSA4096
+                   --avb_vbmeta_system_key "$KEY_DIR/avb.pem" --avb_vbmeta_system_algorithm SHA256_RSA4096)
 fi
 
 if [[ $DEVICE == taimen || $DEVICE == walleye || $DEVICE == blueline || $DEVICE == crosshatch ||
   $DEVICE == sargo || $DEVICE == bonito || $DEVICE == coral || $DEVICE == flame ||
-  $DEVICE == sunfish || $DEVICE == redfin || $DEVICE == bramble ]]; then
+  $DEVICE == sunfish || $DEVICE == redfin || $DEVICE == bramble || $DEVICE == barbet ]]; then
   AVB_PKMD="$PWD/$KEY_DIR/avb_pkmd.bin"
   for apex in "${apexes[@]}"; do
     EXTRA_SIGNING_ARGS+=(--extra_apks $apex=$KEY_DIR/${apex_container_key[$apex]})
