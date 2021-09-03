07b2eeec60e13b707c09845d133e9a0d63698bfc
Chirayu Desai <chirayudesai1@gmail.com>
2021-09-03 23:35:18 +0530

    scripts: Add kebab support
    Change-Id: I6217aba37d774cc8da649446b8b1407ee1c3a16b

--- scripts/updatekeys.sh
+++ scripts/updatekeys.sh
@@ -47,8 +47,9 @@ if [[ ! -e verity.pk8 ]]; then
 fi
 
 if [[ ! -e avb.pem ]]; then
-	if [[ $KEY_DIR =~ barbet || $KEY_DIR =~ oriole || $KEY_DIR =~ raven || $KEY_DIR =~ FP4 ]]; then
-	# AVB 2.0 (Pixel 5a, 6, 6 pro, Fairphone 4)
+	if [[ $KEY_DIR =~ barbet || $KEY_DIR =~ oriole || $KEY_DIR =~ raven || $KEY_DIR =~ FP4 ||
+		$KEY_DIR =~ kebab ]]; then
+	# AVB 2.0 (Pixel 5a, 6, 6 pro, Fairphone 4, OnePlus 8T)
 	openssl genrsa -out avb.pem 4096
 	$AVBTOOL extract_public_key --key avb.pem --output avb_custom_key.img
 	else
