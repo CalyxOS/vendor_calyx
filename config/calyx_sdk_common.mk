910a019497a5499fd838b8ba989925db9c97fa2b
Michael Bestas <mkbestas@gmail.com>
2021-11-10 18:04:08 +0200

    Initial calyx-sdk setup
    Imported from https://github.com/LineageOS/android_vendor_lineage
    at 01d8fe60d8e3f13f8ca37148001a3f76fc60f619
    
    Change-Id: Iaf5f3ae69589c81deba9195132b3cd89b988018e

---
+++ config/calyx_sdk_common.mk
@@ -0,0 +1,16 @@
+# Permissions for calyx sdk services
+PRODUCT_COPY_FILES += \
+    vendor/calyx/config/permissions/org.lineageos.globalactions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.globalactions.xml \
+    vendor/calyx/config/permissions/org.lineageos.hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.hardware.xml \
+    vendor/calyx/config/permissions/org.lineageos.livedisplay.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.livedisplay.xml \
+    vendor/calyx/config/permissions/org.lineageos.settings.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.settings.xml \
+    vendor/calyx/config/permissions/org.lineageos.trust.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.trust.xml
+
+# Calyx Platform Library
+PRODUCT_PACKAGES += \
+    org.lineageos.platform-res \
+    org.lineageos.platform
+
+# JNI Libraries
+PRODUCT_PACKAGES += \
+    liblineage-sdk_platform_jni
