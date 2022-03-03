d797082aa487e00db2f82cedc6c1bed2de10c17e
Tom Pratt <tom.pratt@outlook.com>
2022-03-03 10:54:03 +0100

    Fix GSI targets
    Inherit from AOSP gsi_{arch}.mk, because aosp_{arch}.mk files
    are obsolete in Android 12. Rename for consistency with
    AOSP gsi/sdk targets.
    
    Change-Id: I449390521307ff4f269a9ebf1ad3a5ffc398bc97

--- build/target/product/calyx_gsi_arm64.mk
+++ build/target/product/calyx_gsi_arm64.mk
@@ -12,15 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, build/target/product/aosp_arm.mk)
-$(call inherit-product, build/target/product/gsi_release.mk)
+$(call inherit-product, device/generic/common/gsi_arm64.mk)
 
 $(call inherit-product, vendor/calyx/config/common.mk)
 
+PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
+
 TARGET_NO_KERNEL_OVERRIDE := true
-TARGET_USES_64_BIT_BINDER := true
 
-# Enable mainline checking
-PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
-
-PRODUCT_NAME := calyx_arm
+PRODUCT_NAME := calyx_gsi_arm64
