56a3e83953a5bab31187b6dd66e2986babea8e73
Michael Bestas <mkbestas@gmail.com>
2022-03-04 04:02:28 +0200

    config: Set some better default sounds
    Change-Id: Iddf2ab56e7fbb935f042b00b7cd41d1438500af8

--- config/audio.mk
+++ config/audio.mk
@@ -6,3 +6,9 @@ $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 else
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
 endif
+
+# Default sounds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.alarm_alert=Oxygen.ogg \
+    ro.config.notification_sound=Iapetus.ogg \
+    ro.config.ringtone=Umbriel.ogg
