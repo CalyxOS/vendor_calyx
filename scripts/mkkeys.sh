65dccbce0f0d27819f1ee7e6ede8f73b2434a728
Chirayu Desai <chirayudesai1@gmail.com>
2022-04-07 05:06:37 +0530

    scripts: Support lemonadep
    Change-Id: Id7e5e8ce4050b892b5a719a4538fe9d576996fe7

--- scripts/mkkeys.sh
+++ scripts/mkkeys.sh
@@ -42,8 +42,8 @@ $GENVERITYKEY -convert verity.x509.pem verity_key
 openssl x509 -outform der -in verity.x509.pem -out verity_user.der.x509
 
 if [[ $KEY_DIR =~ barbet || $KEY_DIR =~ oriole || $KEY_DIR =~ raven || $KEY_DIR =~ FP4 ||
-  $KEY_DIR =~ kebab || $KEY_DIR =~ lemonade ]]; then
-# AVB 2.0 (Pixel 5a, 6, 6 pro, Fairphone 4, OnePlus 8T, 9)
+  $KEY_DIR =~ kebab || $KEY_DIR =~ lemonade || $KEY_DIR =~ lemonadep ]]; then
+# AVB 2.0 (Pixel 5a, 6, 6 pro, Fairphone 4, OnePlus 8T, 9, 9 Pro)
 openssl genrsa -out avb.pem 4096
 $AVBTOOL extract_public_key --key avb.pem --output avb_custom_key.img
 else
