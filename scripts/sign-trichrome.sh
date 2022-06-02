b2bae283f67783197fb7e2ee28ebce31ba5c1efe
Chirayu Desai <chirayudesai1@gmail.com>
2022-06-03 02:22:48 +0530

    sign-trichrome: Update APK names to avoid renaming
    Change-Id: Ib741d8a91539bbeae895469223f68051639f263c

--- scripts/sign-trichrome.sh
+++ scripts/sign-trichrome.sh
@@ -13,17 +13,17 @@ KEY=${KEY_DIR}/calyxos.keystore
 error() {
   echo error: $1, please try again >&2
   echo "Usage: $0"
-  echo "Supported files: TrichromeChrome.apk TrichromeLibrary.apk TrichromeWebView.apk"
+  echo "Supported files: (TrichromeChrome6432) universal.apk TrichromeLibrary6432.apk TrichromeWebView6432.apk"
   exit 1
 }
 
 [[ $# -eq 0 ]] || error "incorrect number of arguments"
 
 [[ ! -e ${KEY} ]] && error "$KEY not found"
-[[ ! -e TrichromeChrome.apk ]] && error "TrichromeChrome.aab not found"
-[[ ! -e TrichromeLibrary.apk ]] && error "TrichromeLibrary.apk not found"
-[[ ! -e TrichromeWebView.apk ]] && error "TrichromeWebView.apk not found"
+[[ ! -e universal.apk ]] && error "(TrichromeChrome6432) universal.apk not found"
+[[ ! -e TrichromeLibrary6432.apk ]] && error "TrichromeLibrary6432.apk not found"
+[[ ! -e TrichromeWebView6432.apk ]] && error "TrichromeWebView6432.apk not found"
 
-for APP in TrichromeChrome TrichromeLibrary TrichromeWebView; do
+for APP in universal TrichromeLibrary6432 TrichromeWebView6432; do
 	$APKSIGNER sign --ks ${KEY} --ks-key-alias calyxos --in ${APP}.apk --out ${APP}-signed.apk
 done
