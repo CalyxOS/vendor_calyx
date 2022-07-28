9116f4b94be1c327b8435fb52378eed620f6f77b
Chirayu Desai <chirayudesai1@gmail.com>
2022-07-28 19:12:18 -0400

    Pixel 6a
    Change-Id: I56837d373d4ac8b0cc2b8fb72213ef47aac8e3c1

--- scripts/updatekeys.sh
+++ scripts/updatekeys.sh
@@ -47,9 +47,9 @@ if [[ ! -e verity.pk8 ]]; then
 fi
 
 if [[ ! -e avb.pem ]]; then
-	if [[ $KEY_DIR =~ barbet || $KEY_DIR =~ oriole || $KEY_DIR =~ raven || $KEY_DIR =~ FP4 ||
-		$KEY_DIR =~ kebab || $KEY_DIR =~ lemonade || $KEY_DIR =~ lemonadep ]]; then
-	# AVB 2.0 (Pixel 5a, 6, 6 pro, Fairphone 4, OnePlus 8T, 9, 9 Pro)
+	if [[ $KEY_DIR =~ barbet || $KEY_DIR =~ oriole || $KEY_DIR =~ raven || $KEY_DIR =~ bluejay ||
+		$KEY_DIR =~ FP4 || $KEY_DIR =~ kebab || $KEY_DIR =~ lemonade || $KEY_DIR =~ lemonadep ]]; then
+	# AVB 2.0 (Pixel 5a, 6, 6 pro, 6a, Fairphone 4, OnePlus 8T, 9, 9 Pro)
 	openssl genrsa -out avb.pem 4096
 	$AVBTOOL extract_public_key --key avb.pem --output avb_custom_key.img
 	else
