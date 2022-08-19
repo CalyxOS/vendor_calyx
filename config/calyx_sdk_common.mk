1552fb75017a53ed2e1d0206eb935212fe8f1297
Michael Bestas <mkbestas@gmail.com>
2022-08-19 16:51:28 +0300

    Disable LiveDisplay for now
    Change-Id: I6bc352bf3b7b59292c0bd19fa9c2841161714103

--- config/calyx_sdk_common.mk
+++ config/calyx_sdk_common.mk
@@ -2,7 +2,6 @@
 PRODUCT_COPY_FILES += \
     vendor/calyx/config/permissions/org.lineageos.globalactions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.globalactions.xml \
     vendor/calyx/config/permissions/org.lineageos.hardware.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.hardware.xml \
-    vendor/calyx/config/permissions/org.lineageos.livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.livedisplay.xml \
     vendor/calyx/config/permissions/org.lineageos.settings.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.settings.xml \
     vendor/calyx/config/permissions/org.lineageos.trust.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.trust.xml
 
