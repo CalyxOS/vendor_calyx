CalyxOS/vendor_calyx

commit c48c5155fbf0ed17f8731283838a1fab671b33ca

scripts: Update signing key setup for 13
* This was supposed to get re-written last time yet here we are

Issue: calyxos#1093
Change-Id: I060aee05f2192beab59ab053513a6482a4e3fa4d

scripts/mkkeys.sh
@@ -28,7 +28,11 @@ mkdir -p $KEY_DIR
 
 pushd $KEY_DIR
 
-for k in releasekey platform shared media networkstack com.android.connectivity.resources com.android.hotspot2.osulogin com.android.wifi.resources; do
+for k in releasekey platform shared media networkstack sdk_sandbox \
+		com.android.connectivity.resources \
+		com.android.hotspot2.osulogin com.android.wifi.resources com.android.adservices.api \
+		com.android.bluetooth com.android.safetycenter.resources com.android.wifi.dialog \
+		com.android.uwb.resources com.android.nearby.halfsheet; do
 	$SCRIPTPATH/mkkey.sh "$k" "$SUBJECT"
 done
 

scripts/release.sh
@@ -107,6 +107,12 @@ EXTRA_SIGNING_ARGS+=(-k external/calyx/chromium/certs/chromium=$KEY_DIR/../commo
 EXTRA_SIGNING_ARGS+=(-k packages/modules/Connectivity/service/ServiceConnectivityResources/resources-certs/com.android.connectivity.resources=$KEY_DIR/com.android.connectivity.resources)
 EXTRA_SIGNING_ARGS+=(-k packages/modules/Wifi/OsuLogin/certs/com.android.hotspot2.osulogin=$KEY_DIR/com.android.hotspot2.osulogin)
 EXTRA_SIGNING_ARGS+=(-k packages/modules/Wifi/service/ServiceWifiResources/resources-certs/com.android.wifi.resources=$KEY_DIR/com.android.wifi.resources)
+EXTRA_SIGNING_ARGS+=(-k packages/modules/AdServices/adservices/apk/com.android.adservices.api=$KEY_DIR/com.android.adservices.api)
+EXTRA_SIGNING_ARGS+=(-k packages/modules/Bluetooth/android/app/certs/com.android.bluetooth=$KEY_DIR/com.android.bluetooth)
+EXTRA_SIGNING_ARGS+=(-k packages/modules/Permission/SafetyCenter/Resources/com.android.safetycenter.resources=$KEY_DIR/com.android.safetycenter.resources)
+EXTRA_SIGNING_ARGS+=(-k packages/modules/Wifi/WifiDialog/certs/com.android.wifi.dialog=$KEY_DIR/com.android.wifi.dialog)
+EXTRA_SIGNING_ARGS+=(-k packages/modules/Uwb/service/ServiceUwbResources/resources-certs/com.android.uwb.resources=$KEY_DIR/com.android.uwb.resources)
+EXTRA_SIGNING_ARGS+=(-k packages/modules/Connectivity/nearby/halfsheet/apk-certs/com.android.nearby.halfsheet=$KEY_DIR/com.android.nearby.halfsheet)
 
 if [[ $DEVICE == raven ]]; then
   EXTRA_SIGNING_ARGS+=(-k device/google/gs101-sepolicy/uwb-certs/com.qorvo.uwb=$KEY_DIR/com.qorvo.uwb)

scripts/updatekeys.sh
@@ -27,7 +27,11 @@ AVBTOOL=$TOP/bin/avbtool
 
 pushd $KEY_DIR
 
-for k in releasekey platform shared media networkstack com.android.connectivity.resources com.android.hotspot2.osulogin com.android.wifi.resources; do
+for k in releasekey platform shared media networkstack sdk_sandbox \
+		com.android.connectivity.resources \
+		com.android.hotspot2.osulogin com.android.wifi.resources com.android.adservices.api \
+		com.android.bluetooth com.android.safetycenter.resources com.android.wifi.dialog \
+		com.android.uwb.resources com.android.nearby.halfsheet; do
 	if [[ ! -e ${k}.pk8 ]]; then
 		$SCRIPTPATH/mkkey.sh "$k" "$SUBJECT"
 	fi

scripts/metadata
@@ -30,9 +30,16 @@ keymap[media]=media
 keymap[shared]=shared
 keymap[platform]=platform
 keymap[networkstack]=networkstack
+keymap[sdk_sandbox]=sdk_sandbox
 keymap[com.android.connectivity.resources]=com.android.connectivity.resources
 keymap[com.android.hotspot2.osulogin]=com.android.hotspot2.osulogin
 keymap[com.android.wifi.resources]=com.android.wifi.resources
+keymap[com.android.adservices.api]=com.android.adservices.api
+keymap[com.android.bluetooth]=com.android.bluetooth
+keymap[com.android.safetycenter.resources]=com.android.safetycenter.resources
+keymap[com.android.wifi.dialog]=com.android.wifi.dialog
+keymap[com.android.uwb.resources]=com.android.uwb.resources
+keymap[com.android.nearby.halfsheet]=com.android.nearby.halfsheet
 # Common
 keymap[org.calyxos.fdroid]=org.calyxos.fdroid
 keymap[com.google.android.apps.photos]=com.google.android.apps.photos
@@ -78,9 +85,13 @@ common_app_keys=(
 # APEX keys
 apexes=(
 	com.android.adbd.apex
+	com.android.adservices.apex
 	com.android.appsearch.apex
 	com.android.art.apex
+	com.android.bluetooth.apex
+	com.android.btservices.apex
 	com.android.cellbroadcast.apex
+	com.android.compos.apex
 	com.android.conscrypt.apex
 	com.android.extservices.apex
 	com.android.i18n.apex
@@ -89,6 +100,7 @@ apexes=(
 	com.android.mediaprovider.apex
 	com.android.media.swcodec.apex
 	com.android.neuralnetworks.apex
+	com.android.ondevicepersonalization.apex
 	com.android.os.statsd.apex
 	com.android.permission.apex
 	com.android.resolv.apex
@@ -99,8 +111,10 @@ apexes=(
 	com.android.telephony.apex
 	com.android.tethering.apex
 	com.android.tzdata.apex
+	com.android.uwb.apex
 	com.android.vibrator.drv2624.apex # bonito, sargo
 	com.android.vibrator.sunfish.apex
+	com.android.virt.apex
 	com.android.vndk.current.apex
 	com.android.wifi.apex
 )
@@ -108,9 +122,13 @@ apexes=(
 # Container: x509.pem & pk8
 declare -A apex_container_key
 apex_container_key[com.android.adbd.apex]=com.android.adbd
+apex_container_key[com.android.adservices.apex]=com.android.adservices
 apex_container_key[com.android.appsearch.apex]=com.android.appsearch
 apex_container_key[com.android.art.apex]=com.android.art
+apex_container_key[com.android.bluetooth.apex]=com.android.btservices
+apex_container_key[com.android.btservices.apex]=com.android.btservices
 apex_container_key[com.android.cellbroadcast.apex]=com.android.cellbroadcast
+apex_container_key[com.android.compos.apex]=com.android.compos
 apex_container_key[com.android.conscrypt.apex]=com.android.conscrypt
 apex_container_key[com.android.extservices.apex]=com.android.extservices
 apex_container_key[com.android.i18n.apex]=com.android.i18n
@@ -119,6 +137,7 @@ apex_container_key[com.android.media.apex]=com.android.media
 apex_container_key[com.android.mediaprovider.apex]=com.android.mediaprovider
 apex_container_key[com.android.media.swcodec.apex]=com.android.media.swcodec
 apex_container_key[com.android.neuralnetworks.apex]=com.android.neuralnetworks
+apex_container_key[com.android.ondevicepersonalization.apex]=releasekey
 apex_container_key[com.android.os.statsd.apex]=com.android.os.statsd
 apex_container_key[com.android.permission.apex]=com.android.permission
 apex_container_key[com.android.resolv.apex]=testcert
@@ -129,17 +148,23 @@ apex_container_key[com.android.support.apexer.apex]=releasekey
 apex_container_key[com.android.telephony.apex]=com.android.telephony
 apex_container_key[com.android.tethering.apex]=releasekey
 apex_container_key[com.android.tzdata.apex]=com.android.tzdata
+apex_container_key[com.android.uwb.apex]=com.android.uwb
 apex_container_key[com.android.vibrator.drv2624.apex]=com.android.vibrator.drv2624 # bonito, sargo
 apex_container_key[com.android.vibrator.sunfish.apex]=com.android.vibrator.sunfish
+apex_container_key[com.android.virt.apex]=com.android.virt
 apex_container_key[com.android.vndk.current.apex]=com.android.vndk.current
 apex_container_key[com.android.wifi.apex]=com.android.wifi
 
 # Payload: avbpubkey & pem
 declare -A apex_payload_key
 apex_payload_key[com.android.adbd.apex]=com.android.adbd
+apex_payload_key[com.android.adservices.apex]=com.android.adservices
 apex_payload_key[com.android.appsearch.apex]=com.android.appsearch
 apex_payload_key[com.android.art.apex]=com.android.art
+apex_payload_key[com.android.bluetooth.apex]=com.android.btservices
+apex_payload_key[com.android.btservices.apex]=com.android.btservices
 apex_payload_key[com.android.cellbroadcast.apex]=com.android.cellbroadcast
+apex_payload_key[com.android.compos.apex]=com.android.compos
 apex_payload_key[com.android.conscrypt.apex]=com.android.conscrypt
 apex_payload_key[com.android.extservices.apex]=com.android.extservices
 apex_payload_key[com.android.i18n.apex]=com.android.i18n
@@ -148,6 +173,7 @@ apex_payload_key[com.android.media.apex]=com.android.media
 apex_payload_key[com.android.mediaprovider.apex]=com.android.mediaprovider
 apex_payload_key[com.android.media.swcodec.apex]=com.android.media.swcodec
 apex_payload_key[com.android.neuralnetworks.apex]=com.android.neuralnetworks
+apex_payload_key[com.android.ondevicepersonalization.apex]=com.android.ondevicepersonalization
 apex_payload_key[com.android.os.statsd.apex]=com.android.os.statsd
 apex_payload_key[com.android.permission.apex]=com.android.permission
 apex_payload_key[com.android.resolv.apex]=com.android.resolv
@@ -158,7 +184,9 @@ apex_payload_key[com.android.support.apexer.apex]=com.android.support.apexer
 apex_payload_key[com.android.telephony.apex]=com.android.telephony
 apex_payload_key[com.android.tethering.apex]=com.android.tethering
 apex_payload_key[com.android.tzdata.apex]=com.android.tzdata
+apex_payload_key[com.android.uwb.apex]=com.android.uwb
 apex_payload_key[com.android.vibrator.drv2624.apex]=com.android.vibrator.drv2624 # bonito, sargo
 apex_payload_key[com.android.vibrator.sunfish.apex]=com.android.vibrator.sunfish
+apex_payload_key[com.android.virt.apex]=com.android.virt
 apex_payload_key[com.android.vndk.current.apex]=com.android.vndk.current
 apex_payload_key[com.android.wifi.apex]=com.android.wifi

commit 593603fd053d8a0bc00f024c00691cf9b4fe3d26

sign: Use existing bluetooth key for com.android.bluetooth
* Google put this app inside APEX, and created keys for it
  https://android.googlesource.com/platform/packages/modules/Bluetooth/+/8a2b49337ae47d4eb89af165c824b20540478ce6
* I didn't check them closely and created new keys. Instead,
  it should've used the existing "bluetooth" key as that's
  what this is identical to in AOSP
  i.e.
  build/target/product/security/bluetooth
  packages/modules/Bluetooth/android/app/certs/com.android.bluetooth
  are identical

Fixes: c48c5155fb "scripts: Update signing key setup for 13"
Change-Id: I804c7fba23c0ef173fe4a6ad0b6b16023ea0f7f3

scripts/mkkeys.sh
@@ -31,7 +31,7 @@ pushd $KEY_DIR
 for k in releasekey platform shared media networkstack sdk_sandbox \
 		com.android.connectivity.resources \
 		com.android.hotspot2.osulogin com.android.wifi.resources com.android.adservices.api \
-		com.android.bluetooth com.android.safetycenter.resources com.android.wifi.dialog \
+		com.android.safetycenter.resources com.android.wifi.dialog \
 		com.android.uwb.resources com.android.nearby.halfsheet; do
 	$SCRIPTPATH/mkkey.sh "$k" "$SUBJECT"
 done

scripts/release.sh
@@ -108,7 +108,7 @@ EXTRA_SIGNING_ARGS+=(-k packages/modules/Connectivity/service/ServiceConnectivit
 EXTRA_SIGNING_ARGS+=(-k packages/modules/Wifi/OsuLogin/certs/com.android.hotspot2.osulogin=$KEY_DIR/com.android.hotspot2.osulogin)
 EXTRA_SIGNING_ARGS+=(-k packages/modules/Wifi/service/ServiceWifiResources/resources-certs/com.android.wifi.resources=$KEY_DIR/com.android.wifi.resources)
 EXTRA_SIGNING_ARGS+=(-k packages/modules/AdServices/adservices/apk/com.android.adservices.api=$KEY_DIR/com.android.adservices.api)
-EXTRA_SIGNING_ARGS+=(-k packages/modules/Bluetooth/android/app/certs/com.android.bluetooth=$KEY_DIR/com.android.bluetooth)
+EXTRA_SIGNING_ARGS+=(-k packages/modules/Bluetooth/android/app/certs/com.android.bluetooth=$KEY_DIR/bluetooth)
 EXTRA_SIGNING_ARGS+=(-k packages/modules/Permission/SafetyCenter/Resources/com.android.safetycenter.resources=$KEY_DIR/com.android.safetycenter.resources)
 EXTRA_SIGNING_ARGS+=(-k packages/modules/Wifi/WifiDialog/certs/com.android.wifi.dialog=$KEY_DIR/com.android.wifi.dialog)
 EXTRA_SIGNING_ARGS+=(-k packages/modules/Uwb/service/ServiceUwbResources/resources-certs/com.android.uwb.resources=$KEY_DIR/com.android.uwb.resources)

scripts/updatekeys.sh
@@ -30,7 +30,7 @@ pushd $KEY_DIR
 for k in releasekey platform shared media networkstack sdk_sandbox \
 		com.android.connectivity.resources \
 		com.android.hotspot2.osulogin com.android.wifi.resources com.android.adservices.api \
-		com.android.bluetooth com.android.safetycenter.resources com.android.wifi.dialog \
+		com.android.safetycenter.resources com.android.wifi.dialog \
 		com.android.uwb.resources com.android.nearby.halfsheet; do
 	if [[ ! -e ${k}.pk8 ]]; then
 		$SCRIPTPATH/mkkey.sh "$k" "$SUBJECT"