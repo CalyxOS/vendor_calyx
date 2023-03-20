2efaf4c51f6b8215a502b70698a89f2b308c1437
Chirayu Desai <chirayu@calyxinstitute.org>
2023-03-20 16:37:30 +0000

    Revert "config: Build NavigationBarMode2ButtonOverlay"
    This reverts commit b5b90e3c57b74783e796a0a1489e73250c50513b.
    
    Reason for revert: It's finally broken!
    
    Issue: calyxos#1548
    Change-Id: Id9c79102d66feb7beddb9b3914387a1678bf0d83

--- config/common.mk
+++ config/common.mk
@@ -36,8 +36,7 @@ PRODUCT_PACKAGES += \
     IconShapeSquircleOverlay \
     IconShapeTaperedRectOverlay \
     IconShapeTeardropOverlay \
-    IconShapeVesselOverlay \
-    NavigationBarMode2ButtonOverlay
+    IconShapeVesselOverlay
 
 # Themes
 PRODUCT_PACKAGES += \
