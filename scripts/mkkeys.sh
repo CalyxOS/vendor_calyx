96799c58dbaa9baa45da54ac1495d26383e23b32
Michael Bestas <mkbestas@gmail.com>
2023-04-04 23:30:05 +0300

    scripts: Add devon/hawao/rhode support
    Change-Id: I1edf2de1ca6d4177908d4ef872a0f656ef212303

--- scripts/mkkeys.sh
+++ scripts/mkkeys.sh
@@ -48,8 +48,8 @@ openssl x509 -outform der -in verity.x509.pem -out verity_user.der.x509
 if [[ $KEY_DIR =~ felix || $KEY_DIR =~ tangorpro || $KEY_DIR =~ lynx || $KEY_DIR =~ cheetah || $KEY_DIR =~ panther ||
 	$KEY_DIR =~ barbet || $KEY_DIR =~ oriole || $KEY_DIR =~ raven || $KEY_DIR =~ bluejay ||
 	$KEY_DIR =~ FP4 || $KEY_DIR =~ kebab || $KEY_DIR =~ lemonade || $KEY_DIR =~ lemonadep ||
-	$KEY_DIR =~ axolotl ]]; then
-# AVB 2.0 (Pixel Fold, Tablet, 7a, 7, 7 pro, 5a, 6, 6 pro, 6a, Fairphone 4, OnePlus 8T, 9, 9 Pro, SHIFT6mq)
+	$KEY_DIR =~ axolotl || $KEY_DIR =~ devon || $KEY_DIR =~ hawao || $KEY_DIR =~ rhode ]]; then
+# AVB 2.0 (Pixel Fold, Tablet, 7a, 7, 7 pro, 5a, 6, 6 pro, 6a, Fairphone 4, OnePlus 8T, 9, 9 Pro, SHIFT6mq, moto g32, g42, g52)
 openssl genrsa -out avb.pem 4096
 $AVBTOOL extract_public_key --key avb.pem --output avb_custom_key.img
 else
