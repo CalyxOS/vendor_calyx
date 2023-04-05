2d9ee1bace83bec5d4e1e950426b6095199dca7b
Chirayu Desai <chirayudesai1@gmail.com>
2023-04-05 19:46:48 +0530

    Drop x86 targets, keep x86_64
    * In a world with 64-bit only, let's reduce our work a little
    * No longer building webview for this
    
    Change-Id: Icebae504375eac2366ddcbd8b5a6fdee45130f4c

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -17,15 +17,11 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/calyx_gsi_arm.mk \
     $(LOCAL_DIR)/calyx_gsi_arm64.mk \
-    $(LOCAL_DIR)/calyx_gsi_x86.mk \
     $(LOCAL_DIR)/calyx_gsi_x86_64.mk \
-    $(LOCAL_DIR)/calyx_sdk_phone_x86.mk \
     $(LOCAL_DIR)/calyx_sdk_phone_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
     calyx_gsi_arm-userdebug \
     calyx_gsi_arm64-userdebug \
-    calyx_gsi_x86-userdebug \
     calyx_gsi_x86_64-userdebug \
-    calyx_sdk_phone_x86-userdebug \
     calyx_sdk_phone_x86_64-userdebug
