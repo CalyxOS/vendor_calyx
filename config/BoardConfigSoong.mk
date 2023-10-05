bff1db04524f02454dde6b4f1c20a6aeea02843b
Chirayu Desai <chirayudesai1@gmail.com>
2023-10-06 04:20:25 +0530

    [DO NOT MERGE] Make it build, soong
    Fix the below and more:
    1. FAILED: out/soong/bp2build_files_marker
        config file: /android14/out/soong/soong.variables did not parse correctly: invalid character '/' after object key:value pair
    2. error parsing variable "args": error parsing ninja string
       invalid character after '$' at byte offset 2800
    
    Some of this is invalid json
    
    out/soong/soong.variables:
            "calyxVarsPlugin": {
                "KERNEL_ARCH": "arm64",
                "KERNEL_BUILD_OUT_PREFIX": "/android14/",
                "KERNEL_CROSS_COMPILE": "CROSS_COMPILE=\\"/android14/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-\\" CROSS_COMPILE_ARM32=\\"/android14/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-\\" CROSS_COMPILE_COMPAT=\\"/android14/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-\\"",
                "KERNEL_MAKE_CMD": "/android14/prebuilts/build-tools/linux-x86/bin/make",
                "KERNEL_MAKE_FLAGS": "-j32 CPATH=\\"/usr/include:/usr/include/x86_64-linux-gnu\\" HOSTLDFLAGS=\\"-L/usr/lib/x86_64-linux-gnu -L/usr/lib64 -fuse-ld=lld\\" HOSTCC=/android14/prebuilts/clang/host/linux-x86/clang-r450784d/bin/clang HOSTCXX=/android14/prebuilts/clang/host/linux-x86/clang-r450784d/bin/clang++ LD=/android14/prebuilts/clang/host/linux-x86/clang-r450784d/bin/ld.lld AR=/android14/prebuilts/clang/host/linux-x86/clang-r450784d/bin/llvm-ar LLVM=1 LLVM_IAS=1 LEX=/android14/prebuilts/build-tools/linux-x86/bin/flex YACC=/android14/prebuilts/build-tools/linux-x86/bin/bison M4=/android14/prebuilts/build-tools/linux-x86/bin/m4 PAHOLE=/android14/prebuilts/kernel-build-tools/linux-x86/bin/pahole",
                "PATH_OVERRIDE_SOONG": "PERL5LIB=/android14/prebuilts/tools-lineage/common/perl-base PATH=/android14/prebuilts/tools-lineage/linux-x86/bin:/android14/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:/android14/prebuilts/build-tools/path/linux-x86:/android14/out/.path BISON_PKGDATADIR=/android14/prebuilts/build-tools/common/bison",
                "TARGET_KERNEL_CONFIG": "",
                "TARGET_KERNEL_SOURCE": "build/make/target/board/generic_arm64"
            },
    
    Change-Id: I6c038297669c5ec792b2ce0fcf22adac75a4b390

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -1,16 +1,3 @@
-PATH_OVERRIDE_SOONG := $(shell echo $(TOOLS_PATH_OVERRIDE))
-
-# Add variables that we wish to make available to soong here.
-EXPORT_TO_SOONG := \
-    KERNEL_ARCH \
-    KERNEL_BUILD_OUT_PREFIX \
-    KERNEL_CROSS_COMPILE \
-    KERNEL_MAKE_CMD \
-    KERNEL_MAKE_FLAGS \
-    PATH_OVERRIDE_SOONG \
-    TARGET_KERNEL_CONFIG \
-    TARGET_KERNEL_SOURCE
-
 # Setup SOONG_CONFIG_* vars to export the vars listed above.
 # Documentation here:
 # https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
@@ -23,8 +10,6 @@ define addVar
   SOONG_CONFIG_calyxVarsPlugin += $(1)
   SOONG_CONFIG_calyxVarsPlugin_$(1) := $$(subst ",\",$$($1))
 endef
-
-$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += calyxGlobalVars
 SOONG_CONFIG_calyxGlobalVars += \
