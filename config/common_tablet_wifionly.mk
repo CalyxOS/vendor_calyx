fbc1688aa4d013df198359d570723b84e90201b4
LuK1337 <priv.luk@gmail.com>
2024-02-17 18:27:57 +0100

    config: Ship EmergencyInfo on WiFi only tablets
    Change-Id: I89e16f716076d5351949c0bb13f55fc4eaeee4f5

--- config/common_tablet_wifionly.mk
+++ config/common_tablet_wifionly.mk
@@ -2,3 +2,5 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
 # Inherit common CalyxOS stuff
 $(call inherit-product, vendor/calyx/config/common.mk)
+
+$(call inherit-product, vendor/calyx/config/wifionly.mk)
