d580f68611d22c5ae1002e614c66dd9deeacf728
Michael Bestas <mkbestas@gmail.com>
2024-03-06 12:57:32 +0200

    envsetup: Fix breakfast
    Change-Id: I9742638765f323876bffcdd0d9c1131d28a54614

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -38,6 +38,7 @@ function breakfast()
 {
     target=$1
     local variant=$2
+    local aosp_target_release=$(cat ${ANDROID_BUILD_TOP}/calyx/scripts/vars/aosp_target_release 2>/dev/null)
 
     if [ $# -eq 0 ]; then
         # No arguments, so let's have the full menu
@@ -52,7 +53,7 @@ function breakfast()
                 variant="userdebug"
             fi
 
-            lunch calyx_$target-$variant
+            lunch calyx_$target-$aosp_target_release-$variant
         fi
     fi
     return $?
