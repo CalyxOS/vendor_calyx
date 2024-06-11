20e1b11c440cee017eb81e0618fd91465e123cc3
Oliver Scott <olivercscott@gmail.com>
2024-06-11 17:09:16 -0400

    Don't install FakeStore as system app
    Change-Id: I9c7694d4d754f4e2f302d4fdd503f80649119a1b

--- config/common.mk
+++ config/common.mk
@@ -101,8 +101,7 @@ $(call inherit-product, prebuilts/calyx/fdroid/fdroid-repo.mk)
 # microG
 PRODUCT_PACKAGES += \
     GmsCore \
-    GsfProxy \
-    FakeStore
+    GsfProxy
 
 # Apps
 PRODUCT_PACKAGES += \
