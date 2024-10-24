757b7399b5ae523921a39624cb919bc7e86cf1a3
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-24 18:48:53 +0800

    calyx: products: Add Cuttlefish products
    How-to:
    1. Build and install cuttlefish packages by following [1]
    2. Build CalyxOS image and hosttar
        ```
        source build/envsetup.sh
        breakfast cf_phone_x86_64
        m hosttar updatepackage
        ```
    3. Extract CalyxOS image and hosttar
        ```
        mkdir calyx-cvd
        cd calyx-cvd
        unzip <CalyxOS tree>/out/target/product/vsoc_x86_64/calyx_cf_phone_x86_64-img.zip
        tar xzvf <CalyxOS tree>/out/host/linux-x86/cvd-host_package.tar.gz
        ```
    4. Run it
        ```
        HOME=$PWD ./bin/launch_cvd
        ```
    5. Open https://127.0.0.1:8443 in browser, and enjoy
    
    [1]: https://github.com/google/android-cuttlefish?tab=readme-ov-file#debian-packages
    
    Change-Id: I7d69db7be99d250f02d7e76fd398ebf5c2ca7c5f

---
+++ build/target/product/calyx_cf_phone_arm64.mk
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+$(call inherit-product, device/google/cuttlefish/vsoc_arm64/phone/aosp_cf.mk)
+
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+TARGET_NO_KERNEL_OVERRIDE := true
+
+# Overrides
+PRODUCT_NAME := calyx_cf_phone_arm64
+PRODUCT_MODEL := CalyxOS Cuttlefish phone built for arm64
