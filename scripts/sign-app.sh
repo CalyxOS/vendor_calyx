7ab811ac2dadca3e0795d273db00576f3f179b8c
Tommy Webb <tommy@calyxinstitute.org>
2025-06-21 20:23:58 -0400

    scripts: Better handling of variables and spaces
    Add missing quotes to many spots that ought to have them, and use arrays
    where possible, incorporating the non-array environment values.
    
    Manual fixes + application of https://www.shellcheck.net/wiki/SC2086 .
    
    Change-Id: I855226400e66297aa78db0a154414db210b2bc9f

--- scripts/sign-app.sh
+++ scripts/sign-app.sh
@@ -3,14 +3,14 @@
 
 SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
 
-source $SCRIPTPATH/metadata
+source "$SCRIPTPATH/metadata"
 
 DEVICE=$1
 KEY_DIR=keys/$DEVICE
 APKSIGNER=bin/apksigner
 
 error() {
-  echo error: $1, please try again >&2
+  echo "error: $1, please try again" >&2
   echo "Usage: $0 <device> <app> file.apk"
   echo "Supported apps: ${apps[@]}"
   exit 1
@@ -27,4 +27,4 @@ fi
 APPKEY=${appkey[$APP]}
 KEY=$KEY_DIR/${keymap[$APPKEY]}
 
-$APKSIGNER sign --key ${KEY}.pk8 --cert ${KEY}.x509.pem --in ${APK} --out ${APP}-signed-${DEVICE}.apk
+"$APKSIGNER" sign --key "${KEY}.pk8" --cert "${KEY}.x509.pem" --in "${APK}" --out "${APP}-signed-${DEVICE}.apk"
--- scripts/updatecommonkeys.sh
+++ scripts/updatecommonkeys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 error() {
-  echo error: $1, please try again >&2
+  echo "error: $1, please try again" >&2
   echo "Usage: $0 key_dir subject"
   echo "Example subject: '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'"
   exit 1
@@ -12,7 +12,7 @@ error() {
 SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
 TOP="$SCRIPTPATH/../../.."
 
-source $SCRIPTPATH/metadata
+source "$SCRIPTPATH/metadata"
 
 KEY_DIR=$1
 SUBJECT="$2"
@@ -22,7 +22,7 @@ SUBJECT="$2"
 
 [[ ! -d $KEY_DIR ]] && error "key directory does not exist"
 
-pushd $KEY_DIR
+pushd "$KEY_DIR"
 
 # Convert chromium keystore
 if [[ -e calyxos.keystore ]]; then
@@ -34,7 +34,7 @@ fi
 
 for k in "${common_app_keys[@]}"; do
   if [[ ! -e ${k}.pk8 ]]; then
-    $SCRIPTPATH/mkkey.sh "$k" "$SUBJECT"
+    "$SCRIPTPATH/mkkey.sh" "$k" "$SUBJECT"
   fi
 done
 
