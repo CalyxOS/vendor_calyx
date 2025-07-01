73ba9c0722a90ac7f2140b5318a34789b2ac0e54
Tommy Webb <tommy@calyxinstitute.org>
2025-07-01 15:49:31 -0400

    [WIP] scripts: PKCS#11 signing-related mega-commit
    Needs a *lot* of cleanup and splitting into multiple commits / dropping
    certain things, along with generally more work to do and more testing.
    
    Issue: calyxos#11
    Change-Id: I7dc0ebc184b3b881d483d90874f872349eda2a32

---
+++ scripts/keymapper.id2b.include.sh
@@ -0,0 +1,262 @@
+# id2b: 2-byte IDs for keys (hexadecimal), suitable e.g. for PKCS#11 devices
+# that use such a scheme.
+
+# ID first byte (prefix), as defined in associative arrays and variables below.
+# 00-0f Reserved
+#    10 AOSP shared AVB keys (if any)
+#    11 AOSP shared core keys
+#    12 AOSP shared APEX container keys (single=0x1200)
+#    13 AOSP shared APEX payload keys (single=0x1200)
+#    14 AOSP shared APEX APK keys (single=0x1200)
+#    15 Updatable apps
+#    16 F-Droid /other
+# 17-1f Reserved
+# 20-fe AOSP per-device key prefixes
+#       - Segmented as specified in per_device_key_type_to_offset.
+#    ff Reserved
+
+# ID second byte potentially begins at a given offset, depending on whether it
+# is a per-device key. The key is calculated based on its index in its array
+# added to that offset. So, with an offset of 00, a key that appears at the top
+# of its array (index 0) has a second byte of 00. The eleventh key (index 10)
+# is byte 0a.
+
+# You can use generate_keymap.sh to see which IDs are associated with which keys.
+
+### BEGIN CONFIGURABLE SECTION ###
+# AOSP per-device key starting prefix, as per above.
+device_key_prefix_base=0x20
+
+# Hexadecimal prefix for a key type when the type is shared across devices, or empty if none.
+declare -g -A key_type_to_prefix=(
+  [avb]="10"
+  [core]="11"
+  [apex_container]="12"
+  [apex_payload]="13"
+  [apex_apk]="14"
+  [app]="15"
+)
+
+# Hexadecimal offset for a key type when per-device.
+declare -g -A per_device_key_type_to_offset=(
+  [avb]="00"
+  [core]="10"
+  [apex_container]="30"
+  [apex_payload]="80"
+  [apex_apk]="d0"
+  [app]="00" # N/A
+)
+
+# AVB key consolidation strategy: all, delegated, or none.
+#         all: Every AVB partition is signed with the vbmeta key.
+#   delegated: The vbmeta partition is signed with the vbmeta key, and a single
+#              separate key (ID+1 of the vbmeta key) is used for everything else.
+#        none: Every AVB partition is signed with a unique key.
+key_avb_consolidation=all
+
+# APEX key consolidation strategy: single, combined, or none.
+#     single: A single key is used to sign every APEX container, payload, and APK.
+#             The ID used is that of the first APEX container key.
+#   combined: Every APEX has a key of its own that is used to sign its container,
+#             payload, and APKs.
+#       none: Every APEX has a unique key for its container, payload, and APKs.
+key_apex_consolidation=single
+
+# For each key type, indicate whether or not its associated keys should be generated
+# separately for each device (y) or should instead be shared among all devices (n).
+# "releasekey" is handled specially (see releasekey_type and releasekey_name).
+declare -g -A key_is_per_device=(
+  [avb]=y
+  [releasekey]=y
+  [core]=n
+  [app]=n
+  [apex_container]=n
+  [apex_payload]=n
+  [apex_apk]=n
+)
+
+# If set, allows the release key (OTA key) to refer to the same key as some
+# other key. With releasekey_type=avb and releasekey_name=vbmeta, the release key
+# will be be the same as a device's AVB vbmeta key (avb_custom_key.img).
+releasekey_type=avb
+releasekey_name=vbmeta
+
+# Set payload signer max signature size to 512 to accommodate RSA4096 keys.
+# Otherwise, a payload_signer crash: padded_signature_size >= signature.size() failed.
+# If you don't need keys of such size, you can set this to the default of 256.
+PAYLOAD_SIGNER_MAXIMUM_SIGNATURE_SIZE=512
+### END CONFIGURABLE SECTION ###
+
+# Keymapper files are sourced by release.sh.
+[ -n "${KEY_DIR+x}" ] || export KEY_DIR=keys/
+
+# Set KEY_SUFFIX= (empty) for no key suffix, e.g. for PKCS#11 key labels.
+[ -n "${KEY_SUFFIX+x}" ] || KEY_SUFFIX=.pem
+
+# Mapping of a particular key type to which array (from the metadata file) to use.
+declare -g -A key_type_to_array=(
+  [avb]=keys_avb_partitions
+  [core]=keys_core
+  [apex_container]=keys_apex
+  [apex_payload]=keys_apex
+  [apex_apk]=keys_apex_apk
+  [app]=keys_app
+)
+
+# Returns true if, within a given key type, a given key name should be
+# considered per-device. Contains special handling for the release key.
+get_key_is_per_device_yn() {
+  local key_type=$1
+  local key_name=${2:-}
+  local value
+  if [ "$key_type" = "core" ] && \
+     [ "$key_name" = "build/make/target/product/security/testkey" ]; then
+    key_type=${releasekey_type:-$key_type}
+    key_name=${releasekey_name:-$key_name}
+    value=${key_is_per_device[releasekey]:-${key_is_per_device[$key_type]}}
+  else
+    value=${key_is_per_device[$key_type]:-}
+  fi
+  if [ -z "$value" ]; then
+    echo "ERROR: Key type '$key_type' is not handled by get_key_type_is_per_device_yn." >&2
+    return 1
+  fi
+  printf "%s\n" "$value"
+}
+
+# Returns the base ID for a given key type. The second argument, key name,
+# is necessary to aid in determining if it's a per-device key due to the release
+# key's special handling.
+_get_key_id_base() {
+  local key_type=$1
+  local key_name=$2
+  local per_device
+  per_device=$(get_key_is_per_device_yn "$key_type" "$key_name") || return $?
+  if [ "$per_device" = "y" ]; then
+    local prefix=${all_devices_id_lookup[$DEVICE]:-}
+    if [ -z "$prefix" ]; then
+      echo "ERROR: Device '$DEVICE' not found in all_devices array (check metadata file)." >&2
+      return 1
+    fi
+    local offset=${per_device_key_type_to_offset[$key_type]:-}
+    if [ -z "$offset" ]; then
+      echo "ERROR: Offset not found for key type '$key_type'." >&2
+      return 1
+    fi
+    printf "%x\n" "$((0x$prefix << 8 | 0x$offset))"
+  else
+    local prefix=${key_type_to_prefix[$key_type]:-}
+    if [ -z "$prefix" ]; then
+      echo "ERROR: Prefix not found for key type '$key_type'." >&2
+      return 1
+    fi
+    printf "%x\n" "$((0x$prefix << 8))"
+  fi
+}
+
+# Returns the key ID for the given key type and key name. This ID may not be unique
+get_key_id() {
+  local key_type=$1
+  local key_name=$2
+  local base
+  if [ "$key_type" = "avb" ]; then
+    case "$key_avb_consolidation" in
+      all) key_name=vbmeta ;; # every AVB key uses vbmeta key
+      delegated|other)
+        if [ "$key_name" != "vbmeta" ]; then
+          key_name=${keys_avb_partitions[1]} # just use the second key
+        fi ;;
+      none) true ;; # all discrete AVB keys
+      *) echo "Unknown value '$key_avb_consolidation' for key_avb_consolidation." >&2; exit 1 ;;
+    esac
+  fi
+  if [ "$key_type" = "apex_container" ] || [ "$key_type" = "apex_payload" ] || \
+     [ "$key_type" = "apex_apk" ]; then
+    case "$key_apex_consolidation" in
+      none) true ;; # change nothing
+      combined) key_type=apex_container ;; # just use apex_container
+      single) key_type=apex_container; key_name=${keys_apex[0]} ;; # just use the first key
+      *) echo "Unknown value '$key_apex_consolidation' for key_apex_consolidation." >&2; exit 1 ;;
+    esac
+  fi
+  if [ "$key_type" = "core" ] && \
+     [ "$key_name" = "build/make/target/product/security/testkey" ]; then
+    key_type=$releasekey_type
+    key_name=$releasekey_name
+  fi
+  base=$(_get_key_id_base "$key_type" "$key_name") || return $?
+  local array_name=${key_type_to_array[$key_type]:-}
+  if [ -z "$array_name" ]; then
+    echo "ERROR: Key type '$key_type' is not present in the key_type_to_array map." >&2
+    return 1
+  fi
+  array_name=${array_name}_id_lookup
+  local lookup_reference="${array_name}[$key_name]"
+  local lookup_result="${!lookup_reference:-}"
+  if [ -z "$lookup_result" ]; then
+    echo "ERROR: Could not find '$key_name' in our ID lookup map for the '$array_name' array." >&2
+    return 1
+  fi
+  printf "%x\n" "$((0x$base | 0x$lookup_result))"
+}
+
+get_key() {
+  local key_type=$1
+  local key_name=$2
+  local suffix=${3:-}
+  case "$key_type" in
+    apex_payload|avb)
+      true ;; # keep suffix for these, if any
+    *)
+      suffix= ;; # no suffix for others
+  esac
+  local key_id
+  key_id=$(get_key_id "$key_type" "$key_name") || return $?
+  printf "%s\n" "${KEY_DIR}${key_id}$suffix"
+}
+
+fill_avb_arguments() {
+  local rsa_key_size
+  case "$DEVICE" in
+    redfin|bramble)
+      rsa_key_size=2048 ;;
+    *)
+      rsa_key_size=4096 ;;
+  esac
+  if [ -n "${RSA_KEY_SIZE:-}" ] && [ "$RSA_KEY_SIZE" -lt "$rsa_key_size" ]; then
+    rsa_key_size=$RSA_KEY_SIZE
+  fi
+  avb_algorithm=SHA256_RSA$rsa_key_size
+  avb_arguments=()
+  for partition in "${keys_avb_partitions[@]}"; do
+    avb_arguments+=(
+      "--avb_${partition}_key" "$(get_key avb "$partition" "$KEY_SUFFIX")"
+      "--avb_${partition}_algorithm" "$avb_algorithm"
+    )
+  done
+}
+
+# Create maps that translate a given key into a hexadecimal ID based on the key's position
+# in its array in the metadata file.
+_generate_key_id_maps() {
+  local array_name
+  for array_name in "${key_type_to_array[@]}" all_devices; do
+    local lookup_array_name=${array_name}_id_lookup
+    declare -g -A "${lookup_array_name}=()"
+    local array="${array_name}[@]"
+    local i=0
+    if [ "$array_name" = "all_devices" ]; then
+      i=$(($device_key_prefix_base)) # all_devices is used for ID prefixes.
+    fi
+    local key
+    for key in "${!array}"; do
+      local id=$(printf "%02x" "$i")
+      declare -g "${lookup_array_name}[$key]=$id"
+      i=$((i+1))
+    done
+  done
+}
+_generate_key_id_maps
+
+# Set AVB custom key path for generate-factory-images-common.sh.
+[ -z "${DEVICE:-}" ] || AVB_CUSTOM_KEY=$PWD/$(get_key avb vbmeta).avbpubkey
---
+++ scripts/keymapper.legacy.include.sh
@@ -0,0 +1,188 @@
+# Keymapper files are sourced by release.sh.
+export KEY_DIR=keys/$DEVICE/
+
+get_key() {
+  local key_type=$1
+  shift 1
+  case "$key_type" in
+    avb)
+      _get_key_avb "$@" || return $? ;;
+    core)
+      _get_key_core "$@" || return $? ;;
+    apex_container)
+      _get_key_apex_container "$@" || return $? ;;
+    apex_payload)
+      _get_key_apex_payload "$@" || return $? ;;
+    apex_apk)
+      _get_key_apex_apk "$@" || return $? ;;
+    app)
+      _get_key_app "$@" || return $? ;;
+    *)
+      echo "ERROR: Key type '$key_type' is not handled by get_key." >&2
+      return 1 ;;
+  esac
+}
+
+get_key_is_per_device_yn() {
+  local key_type=$1
+  case "$key_type" in
+    app)
+      echo n ;;
+    avb|core|apex_container|apex_payload|apex_apk)
+      echo y ;;
+    *)
+      echo "ERROR: Key type '$key_type' is not handled by get_key_is_per_device_yn." >&2
+      return 1 ;;
+  esac
+}
+
+_get_key_avb() {
+  local key_suffix=.pem
+  [ -z "${KEY_SUFFIX+x}" ] || key_suffix=$KEY_SUFFIX
+  printf "%s\n" "${KEY_DIR}avb${key_suffix}"
+}
+
+_get_key_core() {
+  local key=$(basename "$1")
+  if [ "$key" = "testkey" ]; then
+    key=releasekey
+  fi
+  printf "%s\n" "${KEY_DIR}$key"
+}
+
+_get_key_apex_container() {
+  local key=$(basename "$1")
+  printf "%s\n" "${KEY_DIR}${apex_container_key[$key]:-$key}"
+}
+
+_get_key_apex_payload() {
+  local key=$(basename "$1")
+  local key_suffix=.pem
+  [ -z "${KEY_SUFFIX+x}" ] || key_suffix=$KEY_SUFFIX
+  printf "%s\n" "${KEY_DIR}${apex_payload_key[$key]:-$key}${key_suffix}"
+}
+
+_get_key_apex_apk() {
+  local key=$1
+  if [ "$key" = "device/google/gs-common/uwb-certs/com.qorvo.uwb" ]; then
+    case "$DEVICE" in
+      raven|cheetah|tangorpro|felix)
+        true ;; # Only map this key for these devices.
+      *)
+        return 0 ;; # Output nothing to indicate we do not want to map this key.
+    esac
+  fi
+  printf "%s\n" "${KEY_DIR}${apex_apk_key[$key]:-$key}"
+}
+
+_get_key_app() {
+  local key=$(basename "$1")
+  printf "%s\n" "${KEY_DIR}../common/$key"
+}
+
+fill_avb_arguments() {
+  case "$DEVICE" in
+    redfin|bramble)
+      avb_arguments=(--avb_vbmeta_key "${KEY_DIR}avb.pem" --avb_vbmeta_algorithm SHA256_RSA2048
+                     --avb_system_key "${KEY_DIR}avb.pem" --avb_system_algorithm SHA256_RSA2048
+                     --avb_vbmeta_system_key "${KEY_DIR}avb.pem" --avb_vbmeta_system_algorithm SHA256_RSA2048)
+      ;;
+    barbet|FP4|FP5|devon|hawao|rhode|fogos|bangkk|fogo|otter)
+      avb_arguments=(--avb_vbmeta_key "${KEY_DIR}avb.pem" --avb_vbmeta_algorithm SHA256_RSA4096
+                     --avb_system_key "${KEY_DIR}avb.pem" --avb_system_algorithm SHA256_RSA4096
+                     --avb_vbmeta_system_key "${KEY_DIR}avb.pem" --avb_vbmeta_system_algorithm SHA256_RSA4096)
+      ;;
+    oriole|raven|bluejay)
+      avb_arguments=(--avb_vbmeta_key "${KEY_DIR}avb.pem" --avb_vbmeta_algorithm SHA256_RSA4096
+                     --avb_system_key "${KEY_DIR}avb.pem" --avb_system_algorithm SHA256_RSA4096
+                     --avb_system_other_key "${KEY_DIR}avb.pem" --avb_system_other_algorithm SHA256_RSA4096
+                     --avb_vbmeta_system_key "${KEY_DIR}avb.pem" --avb_vbmeta_system_algorithm SHA256_RSA4096
+                     --avb_vbmeta_vendor_key "${KEY_DIR}avb.pem" --avb_vbmeta_vendor_algorithm SHA256_RSA4096
+                     --avb_boot_key "${KEY_DIR}avb.pem" --avb_boot_algorithm SHA256_RSA4096)
+      ;;
+    panther|cheetah|lynx|tangorpro|felix|shiba|husky|akita|tokay|caiman|komodo|comet|tegu)
+      avb_arguments=(--avb_vbmeta_key "${KEY_DIR}avb.pem" --avb_vbmeta_algorithm SHA256_RSA4096
+                     --avb_system_key "${KEY_DIR}avb.pem" --avb_system_algorithm SHA256_RSA4096
+                     --avb_system_other_key "${KEY_DIR}avb.pem" --avb_system_other_algorithm SHA256_RSA4096
+                     --avb_vbmeta_system_key "${KEY_DIR}avb_vbmeta_system.pem" --avb_vbmeta_system_algorithm SHA256_RSA4096
+                     --avb_vbmeta_vendor_key "${KEY_DIR}avb.pem" --avb_vbmeta_vendor_algorithm SHA256_RSA4096
+                     --avb_boot_key "${KEY_DIR}avb.pem" --avb_boot_algorithm SHA256_RSA4096
+                     --avb_init_boot_key "${KEY_DIR}avb.pem" --avb_init_boot_algorithm SHA256_RSA4096)
+      ;;
+  esac
+}
+
+declare -gA apex_container_key=(
+  [com.android.tethering]=releasekey
+  [com.android.support.apexer]=releasekey
+  [com.android.hardware.biometrics.face.virtual]=com.android.hardware
+  [com.android.resolv]=testcert
+  [com.android.bluetooth]=com.android.btservices
+  [com.android.hardware.biometrics.fingerprint.virtual]=com.android.hardware
+  [com.android.ondevicepersonalization]=releasekey
+  [com.android.vndk.current.on_vendor]=com.android.vndk.current
+  [com.android.scheduling]=releasekey
+  [com.android.hardware.cas]=com.android.hardware
+)
+
+declare -gA apex_payload_key=(
+  [com.android.hardware.biometrics.face.virtual]=com.android.hardware
+  [com.android.bluetooth]=com.android.btservices
+  [com.android.hardware.biometrics.fingerprint.virtual]=com.android.hardware
+  [com.android.vndk.current.on_vendor]=com.android.vndk.current
+  [com.android.hardware.cas]=com.android.hardware
+)
+
+declare -gA apex_apk_key=(
+  [prebuilts/calyx/microg/certs/microg]=../common/microg
+  [external/calyx/chromium/certs/chromium]=../common/chromium
+  [packages/modules/Connectivity/service/ServiceConnectivityResources/resources-certs/com.android.connectivity.resources]=com.android.connectivity.resources
+  [packages/modules/Wifi/OsuLogin/certs/com.android.hotspot2.osulogin]=com.android.hotspot2.osulogin
+  [packages/modules/Wifi/service/ServiceWifiResources/resources-certs/com.android.wifi.resources]=com.android.wifi.resources
+  [packages/modules/AdServices/adservices/apk/com.android.adservices.api]=com.android.adservices.api
+  [packages/modules/Bluetooth/android/app/certs/com.android.bluetooth]=com.android.bluetooth
+  [packages/modules/Permission/SafetyCenter/Resources/com.android.safetycenter.resources]=com.android.safetycenter.resources
+  [packages/modules/Wifi/WifiDialog/certs/com.android.wifi.dialog]=com.android.wifi.dialog
+  [packages/modules/Uwb/service/ServiceUwbResources/resources-certs/com.android.uwb.resources]=com.android.uwb.resources
+  [packages/modules/Connectivity/nearby/halfsheet/apk-certs/com.android.nearby.halfsheet]=com.android.nearby.halfsheet
+  [packages/providers/MediaProvider/pdf/apk/com.android.graphics.pdf]=com.android.graphics.pdf
+  [build/make/target/product/security/bluetooth]=com.android.bluetooth
+  [build/make/target/product/security/sdk_sandbox]=sdk_sandbox
+  [packages/modules/AppSearch/apk/com.android.appsearch.apk]=com.android.appsearch.apk
+  [packages/modules/HealthFitness/apk/com.android.healthconnect.controller]=com.android.healthconnect.controller
+  [packages/modules/HealthFitness/backuprestore/com.android.health.connect.backuprestore]=com.android.health.connect.backuprestore
+  [build/make/target/product/security/nfc]=com.android.nfcservices
+  [packages/modules/OnDevicePersonalization/federatedcompute/apk/com.android.federatedcompute]=com.android.federatedcompute
+  # Device-specific: raven, cheetah, tangorpro, felix.
+  # (It doesn't hurt to override/generate it anyway.)
+  [device/google/gs-common/uwb-certs/com.qorvo.uwb]=com.qorvo.uwb
+)
+
+# A hack to accommodate the old ways, to ensure keys are inserted in their previous order
+# so that the resulting command line is the same as it once was.
+# (Basically, we need the keys_apex_apk array's items to be in the same order as the keys
+#  in keys_apex_apk above, and bash doesn't store the keys in order...)
+keys_apex_apk_tmp=("${keys_apex_apk[@]}")
+keys_apex_apk=("${keys_app[@]}")
+for key in "${keys_apex_apk_tmp[@]}"; do
+  keys_apex_apk+=("$key")
+  if [ "$key" = "packages/providers/MediaProvider/pdf/apk/com.android.graphics.pdf" ]; then
+    keys_apex_apk+=(
+      build/make/target/product/security/bluetooth
+      build/make/target/product/security/sdk_sandbox
+    )
+  elif [ "$key" = "packages/modules/HealthFitness/backuprestore/com.android.health.connect.backuprestore" ]; then
+    keys_apex_apk+=(
+      build/make/target/product/security/nfc
+    )
+  fi
+done
+
+# Empty out keys_app, keys_avb, keys_core, since they are not used by legacy keymapping.
+# (It was all one big map. We'll use apex_apk_key as that map.)
+keys_avb=()
+keys_core=()
+keys_app=() # Already copied into keys_apex_apk earlier.
+
+# Set AVB custom key path for generate-factory-images-common.sh.
+[ -z "${DEVICE:-}" ] || AVB_CUSTOM_KEY=$PWD/$(dirname "$(get_key avb vbmeta)")/avb_custom_key.img
