6308789b5d310c6ec382da566178501c61b5c29a
Tommy Webb <tommy@calyxinstitute.org>
2025-07-01 15:49:31 -0400

    scripts: PKCS#11 signing-related mega-commit
    Needs a *lot* of cleanup and probably splitting into multiple commits /
    dropping certain things, along with generally more work to do and more
    testing, and of course updating this commit message.
    
    Particularly, the vendor-specific support would ideally be in its own
    commit, along with probably the keygen scripts. This can more easily be
    done later to avoid constant rebasing now, as such components are mostly
    in separate files already.
    
    Issue: calyxos#11
    Issue: calyxos#3409
    Issue: calyxos#3413
    Issue: calyxos#3418
    Change-Id: I7dc0ebc184b3b881d483d90874f872349eda2a32

---
+++ scripts/pkcs11/vendor.yubihsm.intermediary.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -euo pipefail
+ourpath="$(cd "$(dirname "$0")";pwd -P)"
+scriptpath="$(cd "$(dirname "$0")/..";pwd -P)"
+
+export OPENSSL_PKCS11_URI_USES_HEX_KEY_ID=y
+export STRIP_HEX_KEY_ID_PREFIX=y
+num_tries=5
+is_a_signing_command=
+
+main() {
+  declare -g args=()
+  cmd=$SIGNING_COMMAND
+  if [ "$cmd" = "avbtool" ]; then
+    handle_avbtool "$@"
+  elif [ "$cmd" = "java" ]; then
+    handle_java "$@"
+  else
+    args=("$@")
+  fi
+
+  # Note: We expect nothing from stdin, so we don't handle it.
+
+  # Try multiple times, backing off for longer after each try.
+  local err=
+  for try in $(seq 1 $num_tries); do
+    "$cmd" "${args[@]}" || { err=$?; sleep $((try*3 + 2)); continue; }
+    err=0
+    break
+  done
+  if [ "$is_a_signing_command" = "y" ]; then
+    source "$ourpath/vendor.yubihsm.include.sh" || return $?
+
+    PREPEND_LINE="Command:$(printf ' %q' "$SIGNING_COMMAND" "${args[@]}")
+Result: $err" \
+    APPEND_LINE="---" \
+      extract_logs \
+      || return $?
+  fi
+  return $err
+}
+
+handle_avbtool() {
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      --algorithm)
+        # FIXME, temporary
+        if [ $# -gt 1 ] && [ -n "${RSA_KEY_SIZE:-}" ]; then
+          case "$2" in
+            SHA256_RSA*|SHA512_RSA*)
+              if [ "${2:10}" -gt "$RSA_KEY_SIZE" ]; then
+                args+=("$1" "${2:0:10}$RSA_KEY_SIZE")
+                shift 2
+                continue
+              fi ;;
+          esac
+        fi
+        ;;
+      --signing*)
+        is_a_signing_command=y
+        ;;
+    esac
+    args+=("$1")
+    shift 1
+  done
+}
+
+handle_java() {
+  local jar=
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      -jar)
+        jar=$(basename "${2:-}")
+        jar=${jar%.jar}
+        ;;
+    esac
+    args+=("$1")
+    shift 1
+  done
+  if [ "$jar" = "signapk" ]; then
+    is_a_signing_command=y
+  elif [ "$jar" = "apksigner" ]; then
+    is_a_signing_command=y
+  fi
+}
+
+main "$@" || exit $?
