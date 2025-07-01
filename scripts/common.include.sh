6606ecc4d37578ac9b1da371a641f3a6cc166bb8
Tommy Webb <tommy@calyxinstitute.org>
2025-07-01 15:49:31 -0400

    scripts: PKCS#11 signing-related mega-commit
    Needs a *lot* of cleanup and probably splitting into multiple commits /
    dropping certain things, along with generally more work to do and more
    testing, and of course updating this commit message.
    
    Particularly, the vendor-specific support would ideally be in its own
    commit, along with probably the keygen scripts. This can more easily be
    done later to avoid constant rebasing now, as such components are mostly
    in separate files already.
    
    Issue: calyxos#11
    Issue: calyxos#3409
    Issue: calyxos#3413
    Issue: calyxos#3418
    Change-Id: I7dc0ebc184b3b881d483d90874f872349eda2a32

--- scripts/common.include.sh
+++ scripts/common.include.sh
@@ -42,3 +42,78 @@ initialize_keymapper() {
   echo "Not a keymapper file or does not override initialize_keymapper" >&2
   return 1
 }
+
+initialize_release_vendor() {
+  # This method must be overridden by vendor include.
+  echo "Not a vendor include file or does not override initialize_release_vendor" >&2
+  return 1
+}
+
+load_keymapper_and_maybe_pkcs11() {
+  if [ -n "${PKCS11_MODULE:-}" ] || [ -n "${PKCS11_VENDOR:-}" ] || [ -n "${PKCS11_VENDOR_FILE:-}" ]; then
+    source "$scriptpath/pkcs11/include.sh"
+    local should_initialize_vendor=n
+    if [ -n "${PKCS11_VENDOR_FILE:-}" ] || [ -n "${PKCS11_VENDOR:-}" ]; then
+      if [ -n "${PKCS11_VENDOR_FILE:-}" ]; then
+        source "$PKCS11_VENDOR_FILE" || return $?
+      elif [ -n "${PKCS11_VENDOR:-}" ]; then
+        pkcs11_vendor_path=$(cd "$scriptpath"; realpath -e "pkcs11/vendor.${PKCS11_VENDOR}.include.sh" || true)
+        if [ -z "$pkcs11_vendor_path" ]; then
+          echo "Could not find PKCS#11 vendor include script for '$PKCS11_VENDOR'." >&2
+          exit 1
+        fi
+        source "$pkcs11_vendor_path" || return $?
+      fi
+      should_initialize_vendor=y
+    fi
+    load_keymapper || return $?
+    if [ "$should_initialize_vendor" = "y" ]; then
+      initialize_release_vendor || return $?
+    fi
+    tmpdir=$(mktemp -d)
+    cleanup() {
+      rm -f "$tmpdir/sunpkcs11.cfg"
+      rmdir "$tmpdir"
+    }
+    trap cleanup EXIT
+    generate_sunpkcs11_config > "$tmpdir/sunpkcs11.cfg"
+    local tool=${SIGNING_HELPER_TOOL:-openssl}
+    local signing_helper_tool=vendor/calyx/scripts/pkcs11/signing_helper.$tool.sh
+    local payload_signer_tool=vendor/calyx/scripts/pkcs11/payload_signer.$tool.sh
+    if [ ! -e "$signing_helper_tool" ] || [ ! -e "$payload_signer_tool" ]; then
+      echo "Looking for SIGNING_HELPER_TOOL '$tool', missing '$signing_helper_tool' and/or '$payload_signer_tool'" >&2
+      return 1
+    fi
+    EXTRA_RELEASETOOLS_ARGS+=(
+      --verbose
+      --pkcs11_config "$tmpdir/sunpkcs11.cfg"
+      --extra_avbtool_signing_args "--signing_helper $signing_helper_tool"
+    )
+    common_args=(
+      --payload_signer "$payload_signer_tool"
+    )
+    EXTRA_SIGNING_ARGS+=("${common_args[@]}")
+    EXTRA_OTA_ARGS+=("${common_args[@]}")
+  else
+    # Not PKCS#11.
+    load_keymapper || return $?
+  fi
+
+  if [ -n "${SIGNING_COMMAND_INTERMEDIARY:-}" ]; then
+    common_args=(
+      --signing_command_intermediary "$SIGNING_COMMAND_INTERMEDIARY"
+    )
+    EXTRA_SIGNING_ARGS+=("${common_args[@]}")
+    EXTRA_OTA_ARGS+=("${common_args[@]}")
+  fi
+
+  # Payload signer maximum signature size is set to 512 in the metadata file by default
+  # to accommodate RSA4096 keys.
+  if [ -n "${PAYLOAD_SIGNER_MAXIMUM_SIGNATURE_SIZE:-}" ]; then
+    common_args=(
+      --payload_signer_maximum_signature_size "$PAYLOAD_SIGNER_MAXIMUM_SIGNATURE_SIZE"
+    )
+    EXTRA_SIGNING_ARGS+=("${common_args[@]}")
+    EXTRA_OTA_ARGS+=("${common_args[@]}")
+  fi
+}
