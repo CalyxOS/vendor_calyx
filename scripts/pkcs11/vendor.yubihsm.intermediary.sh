4272f1b2b718a1815f9f4798c4673834c2c41240
Tommy Webb <tommy@calyxinstitute.org>
2025-07-01 15:49:31 -0400

    [WIP] scripts: PKCS#11 signing-related mega-commit
    Needs a *lot* of cleanup and splitting into multiple commits / dropping
    certain things, along with generally more work to do and more testing.
    
    Issue: calyxos#11
    Change-Id: I7dc0ebc184b3b881d483d90874f872349eda2a32

---
+++ scripts/pkcs11/vendor.yubihsm.intermediary.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+set -euo pipefail
+
+export OPENSSL_PKCS11_URI_USES_HEX_KEY_ID=y
+num_tries=5
+
+main() {
+  declare -g args=()
+  cmd=$SIGNING_COMMAND
+  if [ "$cmd" = "avbtool" ]; then
+    handle_avbtool "$@"
+  elif [ "$cmd" = "java" ]; then
+    handle_java "$@"
+  else
+    args=("$@")
+  fi
+  { printf "%q " "$cmd" "${args[@]}"; echo; } > last_command.txt
+
+  # Note: We expect nothing from stdin, so we don't handle it.
+
+  # Try multiple times, backing off for longer after each try.
+  local err=
+  for try in $(seq 1 $num_tries); do
+    "$cmd" "${args[@]}" || { err=$?; sleep $((try*3 + 2)); continue; }
+    err=0
+    break
+  done
+  return $err
+}
+
+handle_avbtool() {
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      --algorithm)
+        # FIXME, temporary
+        if [ $# -gt 1 ] && [ -n "${RSA_KEY_SIZE:-}" ]; then
+          case "$2" in
+            SHA256_RSA*|SHA512_RSA*)
+              if [ "${2:10}" -gt "$RSA_KEY_SIZE" ]; then
+                args+=("$1" "${2:0:10}$RSA_KEY_SIZE")
+                shift 2
+                continue
+              fi ;;
+          esac
+        fi
+        ;;
+    esac
+    args+=("$1")
+    shift 1
+  done
+}
+
+handle_java() {
+  local jar=
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      -jar)
+        jar=$(basename "${2:-}")
+        jar=${jar%.jar}
+        ;;
+      --ks-key-alias)
+        if [ $# -gt 1 ] && [ "$jar" = "apksigner" ]; then
+          # Add 0x to the private key.
+          args+=("$1" "0x$2")
+          shift 2
+          continue
+        fi
+        ;;
+    esac
+    args+=("$1")
+    shift 1
+  done
+  if [ "$jar" = "signapk" ]; then
+    # Add 0x to the private key.
+    local private_key_index=$((${#args[@]}-3))
+    args[$private_key_index]=0x${args[$private_key_index]}
+  fi
+}
+
+main "$@"
