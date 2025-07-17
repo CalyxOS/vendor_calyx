47280e6624b428dd1f0ec513d9937398e835f350
Tommy Webb <tommy@calyxinstitute.org>
2025-07-17 16:32:27 -0400

    scripts: Support a DRY_RUN of release scripts
    Run release.sh or generate_delta.sh with DRY_RUN=y and it will merely
    output the commands that would have been run, if this weren't a dry run,
    to stderr. (Does not output commands that are piped to.)
    
    Change-Id: Ia4596aedba4a60c67baef784b1ef890d4d4514dc

---
+++ scripts/common.include.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+scriptpath="$(cd "$(dirname "$0")";pwd -P)"
+
+dry_run() {
+  printf "%q " "$@" >&2
+  echo >&2
+}
+if [ "${DRY_RUN:-}" = "y" ]; then
+  maybe_dry_run=dry_run
+  maybe_dry_run_ignore=true  # runs the command "true" which does and outputs nothing
+else
+  maybe_dry_run=
+  maybe_dry_run_ignore=
+fi
