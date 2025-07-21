10530f58ff30a4d212022b1cae5fc751595b669e
Aaron Kling <webgeek1234@gmail.com>
2025-07-21 00:32:21 -0500

    common: Allow vendors to exclude bp paths
    This resolves a couple issues. Allowing bp's that don't resolve to be
    in-tree. And allowing multiple copies of the same target.
    
    Change-Id: I85cfc566e18ea9fe1cb4cd8d70a71ef80517fd41

--- config/common.mk
+++ config/common.mk
@@ -3,6 +3,9 @@ include vendor/calyx/config/version.mk
 # Exclude repos from bp scanning
 PRODUCT_SOURCE_ROOT_DIRS += -prebuilts/misc/protobuf_vendorcompat
 
+# Allow vendor prebuilt repos to exclude themselves from bp scanning
+-include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
+
 ifneq ($(wildcard vendor/google/gearhead/proprietary/),)
 include device/google/gearhead/device.mk
 include vendor/google/gearhead/gearhead-vendor.mk
