559bdb4d993578c72f46fb3dba112e9d80aff2d4
Tommy Webb <tommy@calyxinstitute.org>
2025-09-09 19:05:16 -0400

    scripts: YubiHSM signing support
    (The info in this commit message is subject to change.)
    
    1) Provisioning wizard
    After loaded onto a USB drive or similar using its prepare-directory
    command, this wizard expects to be run in Tails 6 with an administration
    password specified under Tails' additional settings. You can prepare a
    directory with the necessary files by running:
    
    vendor/calyx/scripts/pkcs11/vendor.yubihsm.provision.sh \
      prepare-directory /path/to/mounted/formatted/flash/drive
    
    The above should be run from an extracted otatools-keys-package or
    after running lunch in an Android build environment. It will create
    a start.sh script that can be executed in Tails from the flash drive
    to begin provisioning.
    
    By the end, the tool will have created a directory in memory,
    /dev/shm/keep, with files that should be copied out of memory and
    stored somewhere safe. These files include logs, exported wrapped keys,
    and attestation certificates.
    
    2) Key generation
    read -p "Enter authkey ID: " -r -s YUBIHSM_AUTHKEY
    read -p "Enter authkey password: " -r -s YUBIHSM_PASSWORD; echo
    export YUBIHSM_AUTHKEY YUBIHSM_PASSWORD
    vendor/calyx/scripts/pkcs11/vendor.yubihsm.keygen.sh [keydir]
    
    3) Signing
    read -p "Enter previous BUILD_NUMBER: " -r PREV_BUILD_NUMBER
    read -p "Enter BUILD_NUMBER: " -r BUILD_NUMBER
    read -p "Enter authkey ID: " -r -s YUBIHSM_AUTHKEY
    read -p "Enter authkey password: " -r -s YUBIHSM_PASSWORD; echo
    export PREV_BUILD_NUMBER BUILD_NUMBER YUBIHSM_AUTHKEY YUBIHSM_PASSWORD
    vendor/calyx/scripts/pkcs11/vendor.yubihsm.release.sh \
      [device] [target_files]
    mv out/* archive/
    vendor/calyx/scripts/pkcs11/vendor.yubihsm.generate_delta.sh \
      [device] "$PREV_BUILD_NUMBER" "$BUILD_NUMBER"
    
    4) Other info
    If you want to use yubihsm-connector outside of the provisioning
    environment, you'll need to start it manually and be sure to export the
    YUBIHSM_CONNECTOR variable before calling keygen or release.
    
    Issue: calyxos#3415
    Issue: calyxos#11
    Change-Id: Ie9273a8d362fbe1c08a88ba91c52c494f95a1c0d

---
+++ scripts/pkcs11/vendor.yubihsm.release.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# WIP
+set -euo pipefail
+pkcs11_scriptpath="$(cd "$(dirname "$0")";pwd -P)"
+scriptpath="$(cd "$(dirname "$0")/..";pwd -P)"
+export SIGNING_COMMAND_INTERMEDIARY=$pkcs11_scriptpath/vendor.yubihsm.intermediary.sh
+export PKCS11_VENDOR=yubihsm
+
+command_to_execute="$scriptpath/release.sh"
+case "$(basename "$0")" in
+  *.release.sh)
+    true ;; # default
+  *.generate_delta.sh)
+    command_to_execute="$scriptpath/generate_delta.sh" ;;
+  *)
+    echo "Warning: The script '$0' has been renamed. Will assume you are using it for release." >&2
+    ;;
+esac
+
+source "$pkcs11_scriptpath/include.sh" || exit $?
+source "$pkcs11_scriptpath/vendor.yubihsm.include.sh" || exit $?
+
+ourtmpdir=
+cleanup() {
+  $maybe_dry_run maybe_stop_apksigner_batch || true
+  $maybe_dry_run maybe_stop_yubihsm_connector || true
+
+  local jobs=$(jobs -p) || true
+  if [ -n "$jobs" ]; then
+    kill $jobs 2>/dev/null || true
+    jobs=$(jobs -p)
+    if [ -n "$jobs" ]; then
+      kill -9 $jobs 2>/dev/null || true
+    fi
+  fi
+
+  if [ -n "$ourtmpdir" ] && [ -d "$ourtmpdir" ]; then
+    rm -rf "$ourtmpdir"
+  fi
+}
+
+if [ "${DRY_RUN:-}" != "y" ]; then
+  trap cleanup EXIT
+
+  ourtmpdir=$(mktemp -d "${TMPDIR:-/tmp}/yhrelease.XXXXXXXX")
+  chmod 0700 "$ourtmpdir"
+
+  if [ -z "${YUBIHSM_PKCS11_CONF:-}" ]; then
+    export YUBIHSM_PKCS11_CONF=$ourtmpdir/yubihsm_pkcs11.conf
+    generate_yubihsm_pkcs11_library_config > "$YUBIHSM_PKCS11_CONF"
+  fi
+
+  maybe_start_yubihsm_connector "$ourtmpdir/yubihsm_connector.pid"
+  maybe_start_apksigner_batch "$ourtmpdir"
+fi
+
+if [ -z "${YUBIHSM_LOGS_DIR+x}" ]; then
+  export YUBIHSM_LOGS_DIR=$(pwd)/logs
+fi
+if [ -n "${YUBIHSM_LOGS_DIR:-}" ]; then
+  if [ ! -d "$YUBIHSM_LOGS_DIR" ]; then
+    $maybe_dry_run mkdir "$YUBIHSM_LOGS_DIR" || {
+      err=$?
+      echo "YUBIHSM_LOGS_DIR ($YUBIHSM_LOGS_DIR) does not exist, and failed to create it" >&2
+      return $err
+    }
+  fi
+  DEVICE=$1
+  default_prefix=$BUILD_NUMBER-$DEVICE-$(get_name_for_hsm_and_session)-$(basename "$command_to_execute" .sh)
+  export AUDIT_LOG_PATH=$YUBIHSM_LOGS_DIR/${AUDIT_LOG_PATH:-$default_prefix-audit.log}
+  export EXEC_LOG_PATH=$YUBIHSM_LOGS_DIR/${EXEC_LOG_PATH:-$default_prefix-exec.log}
+fi
+
+if [ "${DRY_RUN:-}" != "y" ] && [ -n "${EXEC_LOG_PATH:-}" ]; then
+  "$command_to_execute" "$@" 2>&1 | tee -a "$EXEC_LOG_PATH" || exit $?
+  cleanup || true
+  trap "" EXIT
+else
+  "$command_to_execute" "$@" || exit $?
+fi
