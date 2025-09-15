997b0eb07f9b834be9a1459d77c581be06220330
Tommy Webb <tommy@calyxinstitute.org>
2025-09-15 10:56:07 -0400

    scripts: Refactor metadata handling
    Change-Id: I7ff168e8c23946594903b22876f496d11d9f2057

--- scripts/updatekeys.sh
+++ scripts/updatekeys.sh
@@ -15,86 +15,90 @@ SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
 TOP="$SCRIPTPATH/../../.."
 
 source "$SCRIPTPATH/metadata"
+source "$SCRIPTPATH/common.include.sh"
 
-KEY_DIR=$1
+# For legacy reasons, the DEVICE is taken from the path, i.e. keys/DEVICE.
+# The legacy keymapper generates a $KEY_DIR/$DEVICE path which should result in the same path.
+DEVICE=$(basename "$1")
+KEY_DIR=$(dirname "${1%/}")/
 SUBJECT="$2"
 GENVERITYKEY=$TOP/bin/generate_verity_key
 AVBTOOL=$TOP/bin/avbtool
+
+load_keymapper
 
 [[ -e ${GENVERITYKEY} ]] || error "${GENVERITYKEY} not found."
 [[ -e ${AVBTOOL} ]] || error "${AVBTOOL} not found."
 [[ -e $(which openssl) ]] || error "openssl not found in PATH."
 
-[[ -d $KEY_DIR ]] || error "key directory does not exist"
+[[ -d $1 ]] || error "key directory does not exist"
 
-pushd "$KEY_DIR"
-
-for k in releasekey platform shared media networkstack sdk_sandbox \
-    com.android.connectivity.resources \
-    com.android.hotspot2.osulogin com.android.wifi.resources com.android.adservices.api \
-    com.android.bluetooth com.android.safetycenter.resources com.android.wifi.dialog \
-    com.android.uwb.resources com.android.nearby.halfsheet com.android.graphics.pdf \
-    com.android.appsearch.apk com.android.healthconnect.controller \
-    com.android.health.connect.backuprestore com.android.nfcservices \
-    com.android.federatedcompute; do
-  if [[ ! -e ${k}.pk8 ]]; then
-    "$SCRIPTPATH/mkkey.sh" "$k" "$SUBJECT"
+for k in "${keys_core[@]}"; do
+  keyval=$(get_key core "$k" || exit $?)
+  [ -n "$keyval" ] || continue
+  if [[ ! -e ${keyval}.pk8 ]]; then
+    "$SCRIPTPATH/mkkey.sh" "$keyval" "$SUBJECT" \
+      || true  # Broken script. Always returns failure.
+    [[ -e $keyval.pk8 ]] || exit $?  # Exit if the private key does not exist after the above.
   fi
 done
 
-if [[
-  $KEY_DIR =~ raven ||
-  $KEY_DIR =~ cheetah ||
-  $KEY_DIR =~ tangorpro || $KEY_DIR =~ felix
-]]; then
-  if [[ ! -e com.qorvo.uwb.pk8 ]]; then
-    "$SCRIPTPATH/mkkey.sh" "com.qorvo.uwb" "$SUBJECT"
+for k in "${keys_apex_apk[@]}"; do
+  keyval=$(get_key apex_apk "$k" || exit $?)
+  [ -n "$keyval" ] || continue
+  if [[ ! -e ${keyval}.pk8 ]]; then
+    "$SCRIPTPATH/mkkey.sh" "$keyval" "$SUBJECT" \
+      || true  # Broken script. Always returns failure.
+    [[ -e $keyval.pk8 ]] || exit $?  # Exit if the private key does not exist after the above.
   fi
-fi
+done
 
 # AVB 2.0
-if [[ ! -e avb.pem ]]; then
+keyval_vbmeta=$(get_key avb vbmeta || exit $?)
+if [[ ! -e "$keyval_vbmeta" ]]; then
   if [[
     $KEY_DIR =~ redfin || $KEY_DIR =~ bramble
   ]]; then
-    openssl genrsa -out avb.pem 2048
-    "$AVBTOOL" extract_public_key --key avb.pem --output avb_custom_key.img
+    openssl genrsa -out "$keyval_vbmeta" 2048
   else
-    openssl genrsa -out avb.pem 4096
-    "$AVBTOOL" extract_public_key --key avb.pem --output avb_custom_key.img
+    openssl genrsa -out "$keyval_vbmeta" 4096
   fi
 fi
+"$AVBTOOL" extract_public_key --key "$keyval_vbmeta" --output "$AVB_CUSTOM_KEY"
 
-if [[
-  $KEY_DIR =~ panther || $KEY_DIR =~ cheetah || $KEY_DIR =~ lynx || $KEY_DIR =~ tangorpro || $KEY_DIR =~ felix ||
-  $KEY_DIR =~ shiba || $KEY_DIR =~ husky || $KEY_DIR =~ akita ||
-  $KEY_DIR =~ tokay || $KEY_DIR =~ caiman || $KEY_DIR =~ komodo || $KEY_DIR =~ comet || $KEY_DIR =~ tegu
-]]; then
-  if [[ ! -e avb_vbmeta_system.pem ]]; then
-    openssl genrsa -out avb_vbmeta_system.pem 4096
-  fi
+keyval_vbmeta_system=$(get_key avb vbmeta_system || exit $?)
+if [ "$keyval_vbmeta_system" != "$keyval" ]; then
+  openssl genrsa -out "$keyval_vbmeta_system" 4096
 fi
 
-if [[ -e avb_pkmd.bin ]]; then
-  mv avb_pkmd.bin avb_custom_key.img
+if [[ -e "${KEY_DIR%/}/avb_pkmd.bin" ]]; then
+  mv "${KEY_DIR%/}/avb_pkmd.bin" "$AVB_CUSTOM_KEY"
 fi
 
 # Migration from 10 to 11
 # ART apex was renamed, and bionic runtime was split out into a new apex
-if [[ -e com.android.runtime.release.pk8 ]]; then mv com.android.runtime.release.pk8 com.android.runtime.pk8; fi
-if [[ -e com.android.runtime.release.x509.pem ]]; then mv com.android.runtime.release.x509.pem com.android.runtime.x509.pem; fi
+if [[ -e ${KEY_DIR%/}/com.android.runtime.release.pk8 ]]; then
+  mv "${KEY_DIR%/}/com.android.runtime.release.pk8" "${KEY_DIR%/}/com.android.runtime.pk8"
+fi
+if [[ -e ${KEY_DIR%/}/com.android.runtime.release.x509.pem ]]; then
+  mv "${KEY_DIR%/}/com.android.runtime.release.x509.pem" "${KEY_DIR%/}/com.android.runtime.x509.pem"
+fi
 
-for apex in "${apexes[@]}"; do
-  if [[ ! -e ${apex_container_key[$apex]}.pk8 ]]; then
-    "$SCRIPTPATH/mkkey.sh" "${apex_container_key[$apex]}" "$SUBJECT"
+for apex in "${keys_apex[@]}"; do
+  keyval=$(get_key apex_container "$apex" || exit $?)
+  [ -n "$keyval" ] || continue
+  if [[ ! -e $keyval.pk8 ]]; then
+    "$SCRIPTPATH/mkkey.sh" "$keyval" "$SUBJECT" \
+      || true  # Broken script. Always returns failure.
+    [[ -e $keyval.pk8 ]] || exit $?  # Exit if the private key does not exist after the above.
   fi
 done
 
-for apex in "${apexes[@]}"; do
-  if [[ ! -e ${apex_payload_key[$apex]}.pem ]]; then
-    openssl genrsa -out "${apex_payload_key[$apex]}.pem" 4096
-    "$AVBTOOL" extract_public_key --key "${apex_payload_key[$apex]}.pem" --output "${apex_payload_key[$apex]}.avbpubkey"
+for apex in "${keys_apex[@]}"; do
+  keyval=$(KEY_SUFFIX= get_key apex_payload "$apex" || exit $?)
+  [ -n "$keyval" ] || continue
+  if [[ ! -e $keyval.pem ]]; then
+    openssl genrsa -out "$keyval.pem" 4096
+    "$AVBTOOL" extract_public_key --key "$keyval.pem" --output "$keyval.avbpubkey"
   fi
 done
-
-popd
