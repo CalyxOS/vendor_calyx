1b9bbf80c5aedabc8181c39caa26ce1f3ad89f76
Tommy Webb <tommy@calyxinstitute.org>
2025-09-19 15:49:23 -0400

    scripts: Reduce mkkeys scripts redundancy
    The only differences between mkkeys and updatekeys, or mkcommonkeys
    and updatecommonkeys, has been that the former will only proceed if the
    keys directory *does not* exist, and the latter will only proceed if
    the key directory *does* exist, while also performing some migrations
    of preexisting keys if needed.
    
    To simplify this and tackle redundancy, minimize the mk*keys scripts to
    call the update*keys scripts. First, they insist the keys directory must
    not exist. Then, they create it and call the corresponding update*keys
    scripts. In this way, there should be no functional impact as a result
    of this change, but a reduction in maintenance.
    
    We don't show the usage information on this failure, which is a minor
    visible difference that seems like an acceptable loss vs needing to
    keep it up in multiple files.
    
    Change-Id: I96617cec0c5acea4718f721f1eaabc56245b400f

--- scripts/mkcommonkeys.sh
+++ scripts/mkcommonkeys.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
-
-error() {
-  echo error: $1, please try again >&2
-  echo "Usage: $0 key_dir subject"
-  echo "Example subject: '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'"
+set -euo pipefail
+if [ -d "$1" ]; then
+  echo "key directory already exists" >&2
   exit 1
-}
-
-[[ $# -eq 2 ]] ||  error "incorrect number of arguments"
-
-SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
-TOP="$SCRIPTPATH/../../.."
-
-source $SCRIPTPATH/metadata
-
-KEY_DIR=$1
-SUBJECT="$2"
-
-[[ ! -e $(which openssl) ]] && error "openssl not found in PATH."
-
-[[ -d $KEY_DIR ]] && error "key directory already exists"
-mkdir -p $KEY_DIR
-
-pushd $KEY_DIR
-
-for k in "${common_app_keys[@]}"; do
-  $SCRIPTPATH/mkkey.sh "$k" "$SUBJECT"
-done
-
-popd
+fi
+mkdir -p "$1"
+scriptpath=$(cd "$(dirname "$0")" || exit $?;pwd -P)
+exec "$scriptpath/updatecommonkeys.sh" "$@"
--- scripts/mkkeys.sh
+++ scripts/mkkeys.sh
@@ -1,78 +1,9 @@
 #!/bin/bash
-
-error() {
-  echo error: $1, please try again >&2
-  echo "Usage: $0 key_dir subject"
-  echo "Example subject: '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'"
+set -euo pipefail
+if [ -d "$1" ]; then
+  echo "key directory already exists" >&2
   exit 1
-}
-
-[[ $# -eq 2 ]] ||  error "incorrect number of arguments"
-
-SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
-TOP="$SCRIPTPATH/../../.."
-
-source $SCRIPTPATH/metadata
-
-KEY_DIR=$1
-SUBJECT="$2"
-GENVERITYKEY=$TOP/bin/generate_verity_key
-AVBTOOL=$TOP/bin/avbtool
-
-[[ ! -e ${GENVERITYKEY} ]] && error "${GENVERITYKEY} not found."
-[[ ! -e ${AVBTOOL} ]] && error "${AVBTOOL} not found."
-[[ ! -e $(which openssl) ]] && error "openssl not found in PATH."
-
-[[ -d $KEY_DIR ]] && error "key directory already exists"
-mkdir -p $KEY_DIR
-
-pushd $KEY_DIR
-
-for k in releasekey platform shared media networkstack sdk_sandbox \
-    com.android.connectivity.resources \
-    com.android.hotspot2.osulogin com.android.wifi.resources com.android.adservices.api \
-    com.android.bluetooth com.android.safetycenter.resources com.android.wifi.dialog \
-    com.android.uwb.resources com.android.nearby.halfsheet com.android.graphics.pdf \
-    com.android.appsearch.apk com.android.healthconnect.controller \
-    com.android.health.connect.backuprestore com.android.nfcservices \
-    com.android.federatedcompute; do
-  $SCRIPTPATH/mkkey.sh "$k" "$SUBJECT"
-done
-
-if [[
-  $KEY_DIR =~ raven ||
-  $KEY_DIR =~ cheetah ||
-  $KEY_DIR =~ tangorpro || $KEY_DIR =~ felix
-]]; then
-  $SCRIPTPATH/mkkey.sh "com.qorvo.uwb" "$SUBJECT"
 fi
-
-# AVB 2.0
-if [[
-  $KEY_DIR =~ redfin || $KEY_DIR =~ bramble
-]]; then
-  openssl genrsa -out avb.pem 2048
-  $AVBTOOL extract_public_key --key avb.pem --output avb_custom_key.img
-else
-  openssl genrsa -out avb.pem 4096
-  $AVBTOOL extract_public_key --key avb.pem --output avb_custom_key.img
-fi
-
-if [[
-  $KEY_DIR =~ panther || $KEY_DIR =~ cheetah || $KEY_DIR =~ lynx || $KEY_DIR =~ tangorpro || $KEY_DIR =~ felix ||
-  $KEY_DIR =~ shiba || $KEY_DIR =~ husky || $KEY_DIR =~ akita ||
-  $KEY_DIR =~ tokay || $KEY_DIR =~ caiman || $KEY_DIR =~ komodo || $KEY_DIR =~ comet || $KEY_DIR =~ tegu
-]]; then
-  openssl genrsa -out avb_vbmeta_system.pem 4096
-fi
-
-for apex in "${apexes[@]}"; do
-  $SCRIPTPATH/mkkey.sh "${apex_container_key[$apex]}" "$SUBJECT"
-done
-
-for apex in "${apexes[@]}"; do
-  openssl genrsa -out ${apex_payload_key[$apex]}.pem 4096
-  $AVBTOOL extract_public_key --key ${apex_payload_key[$apex]}.pem --output ${apex_payload_key[$apex]}.avbpubkey
-done
-
-popd
+mkdir -p "$1"
+scriptpath=$(cd "$(dirname "$0")" || exit $?;pwd -P)
+exec "$scriptpath/updatekeys.sh" "$@"
