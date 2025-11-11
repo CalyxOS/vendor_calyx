955815e7d7c572e22677bc235b48621b29a48178
Torsten Grote <t@grobox.de>
2025-11-11 17:29:45 -0300

    Add dedicated Python script for log extraction
    Change-Id: I18b431845c9f4872870884f73eb14e517838d7c8

---
+++ scripts/pkcs11/vendor.yubihsm.audit.logs.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import time
+
+from yubihsm import YubiHsm
+
+parser = argparse.ArgumentParser(description="Extracts, saves and uploads YubiHSM 2 audit logs.")
+parser.add_argument("--log-file", help="File to append audit log to", required=True)
+parser.add_argument("--comment", help="Comment for all log entries that will get saved")
+parser.add_argument('-v', '--verbose', action='store_true')
+
+args = parser.parse_args()
+
+
+def main():
+    # connect to the YubiHSM via the connector
+    connector_url = os.getenv("YUBIHSM_CONNECTOR", "http://127.0.0.1:12345")
+    hsm = YubiHsm.connect(connector_url)
+    # establish session
+    auth_key = int(os.getenv("YUBIHSM_AUTHKEY", "0x0001"), 16)
+    password = os.getenv("YUBIHSM_PASSWORD", "password")
+    session = hsm.create_session_derived(auth_key, password)
+    try:
+        extract_and_save_logs(session)
+    finally:
+        session.close()
+
+
+def extract_and_save_logs(session):
+    log_data = session.get_log_entries()
+    if args.comment is None:
+        comment = ""
+    else:
+        comment = args.comment.replace('\n', '').replace('\r', '')
+    if len(log_data.entries) <= 0 and comment == "":
+        print("No logs to extract and no comment to write.")
+        return
+    with open(args.log_file, "a") as file:
+        comment_line = f"# {round(time.time() * 1000)} {comment}"
+        file.write(comment_line + "\n")
+        if args.verbose:
+            print(comment_line)
+        for entry in log_data.entries:
+            entry_str = f"{entry.number:>{5}} cmd: {entry.command:#0{4}x} len: {entry.length:>{5}} sKey: {entry.session_key:#0{6}x} tKey: {entry.target_key:#0{6}x} 2Key: {entry.second_key:#0{6}x} res: {entry.result:#0{4}x} tick: {entry.tick:>{10}} hash: {entry.digest.hex()}"
+            file.write(entry_str + "\n")
+            if args.verbose:
+                print(entry_str)
+    if len(log_data.entries) > 0:
+        last_entry = log_data.entries[-1].number
+        session.set_log_index(last_entry)
+
+
+if __name__ == "__main__":
+    main()
