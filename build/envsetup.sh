9889d312a77cee386f7261909a2045dd6a845266
Michael Bestas <mkbestas@lineageos.org>
2025-11-13 18:11:08 +0200

    build: Fix sourcing envsetup from anywhere
    Change-Id: Id1ee24e89a554c8de42366f9f0f5da03d3c86143

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,7 +1,7 @@
-CLANG_VERSION=$(build/soong/scripts/get_clang_version.py)
+CLANG_VERSION=$(${ANDROID_BUILD_TOP}/build/soong/scripts/get_clang_version.py)
 export LLVM_AOSP_PREBUILTS_VERSION="${CLANG_VERSION}"
 
-RUST_VERSION=$(grep 'RustDefaultVersion =' build/soong/rust/config/global.go | awk '{print $3}' | awk -F '"' '{print $2}')
+RUST_VERSION=$(grep 'RustDefaultVersion =' ${ANDROID_BUILD_TOP}/build/soong/rust/config/global.go | awk '{print $3}' | awk -F '"' '{print $2}')
 export RUST_AOSP_PREBUILTS_VERSION="${RUST_VERSION}"
 
 # Find the top directory
