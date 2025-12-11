5557fdb0af8b35563d618019562192ec9a807bb7
Torsten Grote <t@grobox.de>
2025-12-11 16:23:20 -0300

    scripts: Adapt sign-app.sh script so it can sign with HSM
    Change-Id: I0e3ceb9e202516773e1883b410c1f6c1e8b66185

--- scripts/sign-app.sh
+++ scripts/sign-app.sh
@@ -1,32 +1,78 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Signs given app with the appropriate key
 
 set -euo pipefail
 
 SCRIPTPATH="$(cd "$(dirname "$0")";pwd -P)"
+MAIN_PATH="$(realpath "$SCRIPTPATH/../../../")"
+KEY_DIR="$MAIN_PATH/keys"
 
 source "$SCRIPTPATH/metadata"
+source "$SCRIPTPATH/common.include.sh"
 
-DEVICE=$1
-KEY_DIR=keys/$DEVICE
-APKSIGNER=bin/apksigner
+# ask for username and password
+ask_variable YUBIHSM_AUTHKEY "0x0001" n y || return $?
+ask_variable YUBIHSM_PASSWORD "" y y || return $?
+export YUBIHSM_AUTHKEY YUBIHSM_PASSWORD
+
+source "$SCRIPTPATH/metadata"
+source "$SCRIPTPATH/keymapper.static.include.sh"
+source "$SCRIPTPATH/pkcs11/include.sh"
+source "$SCRIPTPATH/pkcs11/vendor.yubihsm.include.sh"
 
 error() {
   echo "error: $1, please try again" >&2
-  echo "Usage: $0 <device> <app> file.apk"
-  echo "Supported apps: ${apps[@]}"
+  echo "Usage: $0 <app> file.apk"
+  echo "Supported apps: ${keys_app[@]}"
   exit 1
 }
 
-[[ $# -eq 3 ]] || error "incorrect number of arguments"
+[[ $# -eq 2 ]] || error "incorrect number of arguments"
 
-APP=${2}
-APK=${3}
-if [[ ! " ${apps[@]} " =~ " ${APP} " ]]; then
-	error "Unsupported app ${APP}"
+APP=${1}
+APK=${2}
+if [ ! -f "$APK" ]; then
+    error "$APK is not an existing regular file or does not exist." >&2
+fi
+if [[ ! " ${keys_app[@]} " =~ " ${APP} " ]]; then
+  error "Unsupported app ${APP}"
 fi
 
-APPKEY=${appkey[$APP]}
-KEY=$KEY_DIR/$APPKEY
+main() {
+  # get key ID for app
+  KEY_ID=$(get_key_id "app" "$APP")
 
-"$APKSIGNER" sign --key "${KEY}.pk8" --cert "${KEY}.x509.pem" --in "${APK}" --out "${APP}-signed-${DEVICE}.apk"
+  # generate PKCS#11 config for Java
+  SUN_CONFIG_PATH="${TMPDIR:-"/tmp"}/sunpkcs11.cfg"
+  generate_sunpkcs11_config > "$SUN_CONFIG_PATH"
+
+  # disable batch signing, this is just for one app
+  export NEVER_START_APKSIGNER_BATCH="y"
+
+  # this is needed so HSM access will actually work
+  initialize_vendor
+
+  # set file name for audit logging
+  read_yubihsm_deviceinfo || return $?
+  name=$(get_name_for_hsm) || return $?
+  AUDIT_LOG_PATH="$YUBIHSM_LOGS_DIR/$name/$(get_formatted_date)-$APP.log"
+  export AUDIT_LOG_PATH
+
+  # run the actual signing command via the interceptor that also gets used for build signing
+  INTERCEPTOR="$SCRIPTPATH/pkcs11/vendor.yubihsm.interceptor.sh"
+  export SIGNING_COMMAND=java # having to pass the actual command via this variable is an interceptor thing
+  print_and_run "$INTERCEPTOR" \
+    -Xmx4096m \
+    --add-exports=jdk.crypto.cryptoki/sun.security.pkcs11=ALL-UNNAMED \
+    -Djava.library.path="$MAIN_PATH/lib64" \
+    -jar "$MAIN_PATH/framework/apksigner.jar" \
+    sign \
+    --v4-signing-enabled=false --min-sdk-version 30 \
+    --ks NONE --ks-type PKCS11 --ks-pass env:PKCS11_PIN \
+    --provider-class sun.security.pkcs11.SunPKCS11 \
+    --provider-arg "$SUN_CONFIG_PATH" \
+    --ks-key-alias "${KEY_ID}" \
+    --out "${APK%.*}-signed.apk" "${APK}" && echo "Signed $APK successfully"
+}
+
+main "$@"
