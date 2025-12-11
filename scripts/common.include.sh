049c24d0c49cd574186cbf55a940d715ca7838e7
Torsten Grote <t@grobox.de>
2025-12-11 11:16:55 -0300

    scripts: Always use static keymapper with YubiHSM
    This is safer and should protect better against key ID changes in the future.
    
    Also uses a label for the keys while at it.
    
    Change-Id: I3c1ca702f69b50b0ca3f2022df85121eea39cdd9

--- scripts/common.include.sh
+++ scripts/common.include.sh
@@ -28,10 +28,10 @@ load_keymapper() {
   local scriptpath
   scriptpath=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit $?;pwd -P) || return $?
   local keymapper_path
+  local keymapper=${KEYMAPPER:-legacy}
   if [ -n "${KEYMAPPER_FILE:-}" ]; then
     keymapper_path=$(cd "$scriptpath"; realpath -e "$KEYMAPPER_FILE") || return $?
   else
-    local keymapper=${KEYMAPPER:-legacy}
     keymapper_path=$(cd "$scriptpath"; realpath -e "keymapper.$keymapper.include.sh" || true)
     if [ -z "$keymapper_path" ]; then
       echo "Could not find keymapper '$keymapper'." >&2
@@ -48,6 +48,7 @@ load_keymapper() {
     echo "Failed to initialize keymapper: $keymapper_path" >&2
     return $err
   }
+  echo "Using keymapper: $keymapper" >&2
 }
 
 initialize_keymapper() {
