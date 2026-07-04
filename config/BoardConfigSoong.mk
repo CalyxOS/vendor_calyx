f83b5089aa080fe74d2cfc03d769e8c701f83f85
LuK1337 <priv.luk@gmail.com>
2026-07-04 15:33:08 +0200

    config: Export TARGET_MAX_PAGE_SIZE_SUPPORTED to soong
    Change-Id: Ia22d70ee8f80913dda38e601250a45f5604f3deb

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -9,7 +9,8 @@ EXPORT_TO_SOONG := \
     KERNEL_MAKE_FLAGS \
     PATH_OVERRIDE_SOONG \
     TARGET_KERNEL_CONFIG \
-    TARGET_KERNEL_SOURCE
+    TARGET_KERNEL_SOURCE \
+    TARGET_MAX_PAGE_SIZE_SUPPORTED
 
 # Setup SOONG_CONFIG_* vars to export the vars listed above.
 # Documentation here:
