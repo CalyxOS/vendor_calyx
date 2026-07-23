6ed609bb1953b8a5302c37819626db96673c8916
Michael Bestas <mkbestas@gmail.com>
2026-07-23 18:48:58 +0300

    Include frp.img for usage in factory scripts
    Generate frp.img which is fully blank except the last bit which stores
    OEM unlock ability is flipped.
    
    Change-Id: I45033da30254c533aef897563c272edcc1bd2707

---
+++ factory/Android.mk
@@ -0,0 +1,8 @@
+#
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+$(call add-radio-file-sha1-checked,frp.img,eb5a31f973f055e4c72268f33a05cc56275c635b)
